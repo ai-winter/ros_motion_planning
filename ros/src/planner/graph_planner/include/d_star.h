eb6605fd46fbf5a2c95fa280a5155acf484c54bb
Guo_Zhanyu <942568052@qq.com>
2023-01-17 19:45:07 +0800

    to be continued

--- ros/src/planner/graph_planner/include/d_star.h
+++ ros/src/planner/graph_planner/include/d_star.h
@@ -1,83 +1,86 @@
-/***********************************************************
- *
- * @file: d_star.h
- * @breif: Contains the D* planner class
- * @author: Guo Zhanyu
- * @update: 2023-01-14
- * @version: 1.0
- *
- * Copyright (c) 2023， Guo Zhanyu
- * All rights reserved.
- * --------------------------------------------------------
- *
- **********************************************************/
 
 #ifndef D_STAR_H
 #define D_STAR_H
 
+#include <map>
+#include <algorithm>
+
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+
 #include "global_planner.h"
-#include "utils.h"
 
-/**
- * @brief Node class for d_star
- * @param x X value
- * @param y Y value
- * @param cost Cost to get to this node
- * @param h_cost Heuritic cost of this node
- * @param id Node's id
- * @param pid Node's parent's id
- * @param tag Node's state
- * @param k Node's minimum h_cost in history
- */
-class DNode : public Node
+#define inf 1 << 20
+
+enum Tag
 {
-public:
-    // Node's state
-    int tag;
-    // Node's minimum h_cost in history
-    double k;
-
-public:
-    DNode(const int x = 0, const int y = 0, const double cost = 0,
-          const double h_cost = 0, const int id = 0, const int pid = 0,
-          const int tag = 0, const double k = 0)
-        : Node(x, y, cost, h_cost, id, pid), tag(tag), k(k) {}
-
-    /**
-     * @brief Overloading operator + for DNode class
-     * @param p node
-     * @return Node with current node's and input node p's values added
-     */
-    DNode operator+(const DNode &p) const;
+    NEW = 0,
+    OPEN = 1,
+    CLOSED = 2
 };
 
 namespace d_star_planner
 {
-    /**
-     * @brief Class for objects that plan using the A* algorithm
-     */
+    class DNode;
+    typedef DNode *DNodePtr;
+
     class DStar : public global_planner::GlobalPlanner
     {
     public:
-        /**
-         * @brief  Constructor
-         * @param   nx          pixel number in costmap x direction
-         * @param   ny          pixel number in costmap y direction
-         * @param   resolution  costmap resolution
-         */
-        DStar(int nx, int ny, double resolution);
+        // global costmap
+        const unsigned char *global_costmap;
+        // local costmap pointer pointer
+        nav_msgs::OccupancyGrid **pp_local_costmap;
+        // init plan flag
+        bool init_plan;
+        // grid map
+        DNodePtr **DNodeMap;
+        // open list
+        std::multimap<double, DNodePtr> open_list;
+        // path
+        std::vector<Node> path;
 
-        /**
-         * @brief D* implementation
-         * @param costs     costmap
-         * @param start     start node
-         * @param goal      goal node
-         * @param expand    containing the node been search during the process
-         * @return tuple contatining a bool as to whether a path was found, and the path
-         */
+    public:
+        DStar(int nx, int ny, double resolution, nav_msgs::OccupancyGrid *p_local_costmap);
+
+        void reset();
+
+        void initMap();
+
+        void insert(DNodePtr nodePtr, double h_new);
+
+        bool isCollision(DNodePtr n1, DNodePtr n2);
+
+        void getNeighbours(DNodePtr nodePtr, std::vector<DNodePtr> &neighbours);
+
+        double cost(DNodePtr n1, DNodePtr n2);
+
+        double processState();
+
+        void extractExpand(std::vector<Node> &expand);
+
+        void extractPath(const Node &start, const Node &goal);
+
         std::tuple<bool, std::vector<Node>> plan(const unsigned char *costs, const Node &start,
                                                  const Node &goal, std::vector<Node> &expand);
     };
+
+    class DNode : public Node
+    {
+    public:
+        // Node's tag among enum Tag
+        int tag;
+        // Node's k_min in history
+        double k;
+        // Node's iterator from multimap
+        std::multimap<double, DNodePtr>::iterator nodeMapIt;
+
+    public:
+        DNode(const int x = 0, const int y = 0, const double cost = 0, const double h_cost = 0,
+              const int id = 0, const int pid = 0, const int tag = NEW, const double k = 0)
+            : Node(x, y, cost, h_cost, id, pid), tag(tag), k(k) {}
+    };
+
 }
 
 #endif
--- ros/src/planner/graph_planner/src/d_star.cpp
+++ ros/src/planner/graph_planner/src/d_star.cpp
@@ -1,54 +1,225 @@
-/***********************************************************
- *
- * @file: d_star.cpp
- * @breif: Contains the D* planner class
- * @author: Guo Zhanyu
- * @update: 2023-01-14
- * @version: 1.0
- *
- * Copyright (c) 2023， Yang Haodong
- * All rights reserved.
- * --------------------------------------------------------
- *
- **********************************************************/
-
 #include "d_star.h"
-
-/**
- * @brief Overloading operator + for Node class
- * @param p node
- * @return Node with current node's and input node p's values added
- */
-DNode DNode::operator+(const DNode &p) const
-{
-    DNode tmp;
-    tmp.x = this->x + p.x;
-    tmp.y = this->y + p.y;
-    tmp.h_cost = this->h_cost + p.h_cost;
-    return tmp;
-}
 
 namespace d_star_planner
 {
-    /**
-     * @brief  Constructor
-     * @param   nx          pixel number in costmap x direction
-     * @param   ny          pixel number in costmap y direction
-     * @param   resolution  costmap resolution
-     */
-    DStar::DStar(int nx, int ny, double resolution) : GlobalPlanner(nx, ny, resolution) {}
+    DStar::DStar(int nx, int ny, double resolution, nav_msgs::OccupancyGrid *p_local_costmap)
+        : global_planner::GlobalPlanner(nx, ny, resolution)
+    {
+        this->init_plan = false;
 
-    /**
-     * @brief D* implementation
-     * @param costs     costmap
-     * @param start     start node
-     * @param goal      goal node
-     * @param expand    containing the node been search during the process
-     * @return tuple contatining a bool as to whether a path was found, and the path
-     */
+        this->pp_local_costmap = new nav_msgs::OccupancyGrid *;
+        *(this->pp_local_costmap) = p_local_costmap;
+
+        initMap();
+    }
+
+    void DStar::initMap()
+    {
+        this->DNodeMap = new DNodePtr *[this->nx_];
+        for (int i = 0; i < this->nx_; i++)
+        {
+            this->DNodeMap[i] = new DNodePtr[this->ny_];
+            for (int j = 0; j < this->ny_; j++)
+                this->DNodeMap[i][j] = new DNode(i, j, inf, inf, this->grid2Index(i, j));
+        }
+    }
+
+    void DStar::reset()
+    {
+        this->open_list.clear();
+
+        for (int i = 0; i < this->nx_; i++)
+            for (int j = 0; j < this->ny_; j++)
+                delete this->DNodeMap[i][j];
+
+        for (int i = 0; i < this->nx_; i++)
+            delete[] this->DNodeMap[i];
+
+        delete[] this->DNodeMap;
+
+        initMap();
+    }
+
+    void DStar::insert(DNodePtr nodePtr, double h_new)
+    {
+        if (nodePtr->tag == NEW)
+            nodePtr->k = h_new;
+        else if (nodePtr->tag == OPEN)
+            nodePtr->k = std::min(nodePtr->k, h_new);
+        else if (nodePtr->tag == CLOSED)
+            nodePtr->k = std::min(nodePtr->cost, h_new);
+
+        nodePtr->cost = h_new;
+        nodePtr->tag = OPEN;
+        nodePtr->nodeMapIt = this->open_list.insert(std::make_pair(nodePtr->k, nodePtr));
+    }
+
+    bool DStar::isCollision(DNodePtr n1, DNodePtr n2)
+    {
+        int n1_id = n1->id, n2_id = n2->id;
+        return this->global_costmap[n1_id] > this->lethal_cost_ * this->factor_ ||
+               this->global_costmap[n2_id] > this->lethal_cost_ * this->factor_;
+    }
+
+    void DStar::getNeighbours(DNodePtr nodePtr, std::vector<DNodePtr> &neighbours)
+    {
+        int x = nodePtr->x, y = nodePtr->y;
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0)
+                    continue;
+
+                DNodePtr neigbourPtr = this->DNodeMap[x + i][y + j];
+                if (isCollision(nodePtr, neigbourPtr))
+                    continue;
+                if (neigbourPtr->id < 0 || neigbourPtr->id >= this->ns_)
+                    continue;
+
+                neighbours.push_back(neigbourPtr);
+            }
+        }
+    }
+
+    double DStar::cost(DNodePtr n1, DNodePtr n2)
+    {
+        if (isCollision(n1, n2))
+            return inf;
+        return std::sqrt(std::pow(n1->x - n2->x, 2) + std::pow(n1->y - n2->y, 2));
+    }
+
+    double DStar::processState()
+    {
+        if (this->open_list.empty())
+            return -1;
+
+        double k_old = this->open_list.begin()->first;
+        DNodePtr x = this->open_list.begin()->second;
+        this->open_list.erase(this->open_list.begin());
+        x->tag = CLOSED;
+
+        std::vector<DNodePtr> neigbours;
+        this->getNeighbours(x, neigbours);
+
+        if (k_old < x->cost)
+        {
+            for (int i = 0; i < (int)neigbours.size(); i++)
+            {
+                DNodePtr y = neigbours[i];
+                if (y->cost <= k_old && x->cost > y->cost + this->cost(y, x))
+                {
+                    x->pid = y->id;
+                    x->cost = y->cost + this->cost(y, x);
+                }
+            }
+        }
+
+        if (k_old == x->cost)
+        {
+            for (int i = 0; i < (int)neigbours.size(); i++)
+            {
+                DNodePtr y = neigbours[i];
+                if (y->tag == NEW ||
+                    (y->pid == x->id && y->cost != x->cost + this->cost(x, y)) ||
+                    (y->pid != x->id && y->cost > x->cost + this->cost(x, y)))
+                {
+                    y->pid = x->id;
+                    this->insert(y, x->cost + this->cost(x, y));
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < (int)neigbours.size(); i++)
+            {
+                DNodePtr y = neigbours[i];
+
+                if (y->tag == NEW || (y->pid == x->id && y->cost != x->cost + this->cost(x, y)))
+                {
+                    y->pid = x->id;
+                    this->insert(y, x->cost + this->cost(x, y));
+                }
+                else
+                {
+                    if (y->pid != x->id && y->cost > x->cost + this->cost(x, y))
+                    {
+                        this->insert(x, x->cost);
+                    }
+                    else
+                    {
+                        if (y->pid != x->id && x->cost > y->cost + this->cost(x, y) &&
+                            y->tag == CLOSED && y->cost > k_old)
+                        {
+                            this->insert(y, y->cost);
+                        }
+                    }
+                }
+            }
+        }
+
+        return open_list.begin()->first;
+    }
+
+    void DStar::extractExpand(std::vector<Node> &expand)
+    {
+        for (int i = 0; i < this->nx_; i++)
+        {
+            for (int j = 0; j < this->ny_; j++)
+            {
+                DNodePtr tmp = this->DNodeMap[i][j];
+                if (tmp->tag == CLOSED)
+                    expand.push_back(*tmp);
+            }
+        }
+    }
+
+    void DStar::extractPath(const Node &start, const Node &goal)
+    {
+        this->path.clear();
+        DNodePtr nPtr = this->DNodeMap[start.x][start.y];
+        while (nPtr->x != goal.x && nPtr->y != goal.y)
+        {
+            this->path.push_back(*nPtr);
+
+            int x, y;
+            this->index2Grid(nPtr->pid, x, y);
+            nPtr = this->DNodeMap[x][y];
+        }
+        std::reverse(this->path.begin(), this->path.end());
+    }
+
     std::tuple<bool, std::vector<Node>> DStar::plan(const unsigned char *costs, const Node &start,
                                                     const Node &goal, std::vector<Node> &expand)
     {
-        return {false, {}};
+        // ===== NOTE: TEMP, need to rewrite when incroperating local costmap =====
+        expand.clear();
+        this->reset();
+        // ========================================================================
+        this->global_costmap = costs;
+        DNodePtr sPtr = this->DNodeMap[start.x][start.y];
+        DNodePtr gPtr = this->DNodeMap[goal.x][goal.y];
+
+        // if (!this->init_plan)
+        if (1)
+        {
+            this->init_plan = true;
+            gPtr->cost = 0;
+            this->insert(gPtr, gPtr->cost);
+
+            while (1)
+            {
+                processState();
+                if (sPtr->tag == CLOSED)
+                    break;
+            }
+
+            this->extractExpand(expand);
+            this->extractPath(start, goal);
+            return {true, this->path};
+        }
+        // else
+        // {
+        // }
     }
 }
