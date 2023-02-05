67e974b84327e29c40a1c99381ff53737abe7862
Guo_Zhanyu <942568052@qq.com>
2023-02-05 10:47:58 +0800

    add .clang-format, please refer to README.md to format the code

--- ros/src/planner/graph_planner/include/d_star.h
+++ ros/src/planner/graph_planner/include/d_star.h
@@ -1,92 +1,90 @@
-
 #ifndef D_STAR_H
 #define D_STAR_H
-
-#include <map>
-#include <algorithm>
 
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
 
+#include <map>
+#include <algorithm>
+
 #include "global_planner.h"
 
-#define inf 1 << 20
+#define inf 10000
 
 enum Tag
 {
-    NEW = 0,
-    OPEN = 1,
-    CLOSED = 2
+  NEW = 0,
+  OPEN = 1,
+  CLOSED = 2
 };
 
 namespace d_star_planner
 {
-    class DNode;
-    typedef DNode *DNodePtr;
+class DNode;
+typedef DNode* DNodePtr;
 
-    class DStar : public global_planner::GlobalPlanner
-    {
-    public:
-        // global costmap
-        unsigned char *global_costmap;
-        // local costmap pointer pointer
-        nav_msgs::OccupancyGrid **pp_local_costmap;
-        // init plan flag
-        bool init_plan;
-        // grid map
-        DNodePtr **DNodeMap;
-        // open list
-        std::multimap<double, DNodePtr> open_list;
-        // path
-        std::vector<Node> path;
+class DStar : public global_planner::GlobalPlanner
+{
+public:
+  // global costmap
+  unsigned char* global_costmap;
+  // grid map
+  DNodePtr** DNodeMap;
+  // open list
+  std::multimap<double, DNodePtr> open_list;
+  // path
+  std::vector<Node> path;
+  // goal
+  Node goal_;
+  // forward distance, range of the sensor (in grid)
+  int N_;
 
-    public:
-        DStar(int nx, int ny, double resolution, nav_msgs::OccupancyGrid *p_local_costmap);
+public:
+  DStar(int nx, int ny, double resolution);
 
-        void reset();
+  void reset();
 
-        void initMap();
+  void initMap();
 
-        void insert(DNodePtr nodePtr, double h_new);
+  void insert(DNodePtr nodePtr, double h_new);
 
-        bool isCollision(DNodePtr n1, DNodePtr n2);
+  bool isCollision(DNodePtr n1, DNodePtr n2);
 
-        void getNeighbours(DNodePtr nodePtr, std::vector<DNodePtr> &neighbours);
+  void getNeighbours(DNodePtr nodePtr, std::vector<DNodePtr>& neighbours);
 
-        double cost(DNodePtr n1, DNodePtr n2);
+  double cost(DNodePtr n1, DNodePtr n2);
 
-        double processState();
+  double processState();
 
-        void extractExpand(std::vector<Node> &expand);
+  void extractExpand(std::vector<Node>& expand);
 
-        void extractPath(const Node &start, const Node &goal);
+  void extractPath(const Node& start, const Node& goal);
 
-        void updateMap();
+  Node getState(const Node& current);
 
-        Node getState(const Node& current);
+  void modify(DNodePtr x, DNodePtr y);
 
-        void modify(DNodePtr x, DNodePtr y);
+  std::tuple<bool, std::vector<Node>> plan(const unsigned char* costs, const Node& start, const Node& goal,
+                                           std::vector<Node>& expand);
+};
 
-        std::tuple<bool, std::vector<Node>> plan(const unsigned char *costs, const Node &start,
-                                                 const Node &goal, std::vector<Node> &expand);
-    };
+class DNode : public Node
+{
+public:
+  // Node's tag among enum Tag
+  int tag;
+  // Node's k_min in history
+  double k;
+  // Node's iterator from multimap, default from small to large
+  std::multimap<double, DNodePtr>::iterator nodeMapIt;
 
-    class DNode : public Node
-    {
-    public:
-        // Node's tag among enum Tag
-        int tag;
-        // Node's k_min in history
-        double k;
-        // Node's iterator from multimap
-        std::multimap<double, DNodePtr>::iterator nodeMapIt;
-
-    public:
-        DNode(const int x = 0, const int y = 0, const double cost = 0, const double h_cost = 0,
-              const int id = 0, const int pid = 0, const int tag = NEW, const double k = 0)
-            : Node(x, y, cost, h_cost, id, pid), tag(tag), k(k) {}
-    };
-
-}
+public:
+  DNode(const int x = 0, const int y = 0, const double cost = inf, const double h_cost = inf, const int id = 0,
+        const int pid = -1, const int tag = NEW, const double k = inf)
+    : Node(x, y, cost, h_cost, id, pid), tag(tag), k(k)
+  {
+  }
+};
+}  // namespace d_star_planner
 
 #endif
