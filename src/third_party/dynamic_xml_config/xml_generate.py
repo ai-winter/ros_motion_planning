6af3821a9f8969fa9048055d9fe760c0448c6064
Guo_Zhanyu <942568052@qq.com>
2023-03-18 00:23:12 +0800

    update format

--- src/third_party/dynamic_xml_config/xml_generate.py
+++ src/third_party/dynamic_xml_config/xml_generate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-''' 
+""" 
 ******************************************************************************************
 *  Copyright (C) 2023 Yang Haodong, All Rights Reserved                                  *
 *                                                                                        *
@@ -11,7 +11,7 @@
 *  @date     2023.03.15                                                                  *
 *  @license  GNU General Public License (GPL)                                            *
 ******************************************************************************************
-'''
+"""
 import yaml
 import xml.etree.ElementTree as ET
 import sys, os
@@ -22,18 +22,17 @@ class XMLGenerator(object):
         # get the root path of package(src layer)
         self.root_path = os.path.split(os.path.realpath(__file__))[0] + "/../../"
         # user configure
-        self.user_cfg = XMLGenerator.yamlParser(self.root_path + 'user_config/' + sys.argv[1])
-
+        self.user_cfg = XMLGenerator.yamlParser(self.root_path + "user_config/" + sys.argv[1])
 
     def writeEnvMainXml(self, path):
-        '''
+        """
         Create configure file of package `sim_env` dynamically.
 
         Parameters
         ----------
         path: str
             the path of file(.launch.xml) to write.
-        '''
+        """
         # root
         launch = ET.Element("launch")
 
@@ -48,29 +47,36 @@ class XMLGenerator(object):
         include.append(XMLGenerator.createElement("arg", props={"name": "map", "value": self.user_cfg["map"]}))
         include.append(XMLGenerator.createElement("arg", props={"name": "robot_number", "value": str(len(self.user_cfg["robots_config"]))}))
         include.append(XMLGenerator.createElement("arg", props={"name": "rviz_file", "value": self.user_cfg["rviz_file"]}))
-        
+
         launch.append(include)
         XMLGenerator.indent(launch)
 
-        with open(path, 'wb+') as f:
-            ET.ElementTree(launch).write(f, encoding='utf-8', xml_declaration=True)
+        with open(path, "wb+") as f:
+            ET.ElementTree(launch).write(f, encoding="utf-8", xml_declaration=True)
 
     def writeRobotsXml(self, path):
-        '''
+        """
         Create configure file to start robots dynamically.
 
         Parameters
         ----------
         path: str
             the path of file(.launch.xml) to write.
-        '''
-        def getRobotArg(name: str, index: int=-1):
+        """
+
+        def getRobotArg(name: str, index: int = -1):
             if index == -1:
-                e = XMLGenerator.createElement("arg", props={"name": name, \
-                    "value": "$(eval arg('robot' + str(arg('robot_number')) + '_" + name + "'))"})
+                e = XMLGenerator.createElement(
+                    "arg", props={"name": name, "value": "$(eval arg('robot' + str(arg('robot_number')) + '_" + name + "'))"}
+                )
             else:
-                e = XMLGenerator.createElement("arg", props={"name": "robot" + str(index + 1) + "_" + name, \
-                    "value": self.user_cfg["robots_config"][index]["robot" + str(index + 1) + "_" + name]})
+                e = XMLGenerator.createElement(
+                    "arg",
+                    props={
+                        "name": "robot" + str(index + 1) + "_" + name,
+                        "value": self.user_cfg["robots_config"][index]["robot" + str(index + 1) + "_" + name],
+                    },
+                )
             return e
 
         # root
@@ -96,8 +102,9 @@ class XMLGenerator(object):
 
         # create starting node
         include = XMLGenerator.createElement("include", props={"file": "$(find sim_env)/launch/app/environment_single.launch.xml"})
-        include.append(XMLGenerator.createElement("arg", props={"name": "robot", \
-            "value": "$(eval arg('robot' + str(arg('robot_number')) + '_type'))"}))
+        include.append(
+            XMLGenerator.createElement("arg", props={"name": "robot", "value": "$(eval arg('robot' + str(arg('robot_number')) + '_type'))"})
+        )
         # planner
         include.append(getRobotArg("global_planner"))
         include.append(getRobotArg("local_planner"))
@@ -108,38 +115,44 @@ class XMLGenerator(object):
         else:
             include.append(XMLGenerator.createElement("arg", props={"name": "start_ns", "value": "false"}))
         # pose
-        include.append(XMLGenerator.createElement("arg", props={"name": "robot_x", \
-            "value": "$(eval arg('robot' + str(arg('robot_number')) + '_x_pos'))"}))
-        include.append(XMLGenerator.createElement("arg", props={"name": "robot_y", \
-            "value": "$(eval arg('robot' + str(arg('robot_number')) + '_y_pos'))"}))
-        include.append(XMLGenerator.createElement("arg", props={"name": "robot_z", \
-            "value": "$(eval arg('robot' + str(arg('robot_number')) + '_z_pos'))"}))
-        include.append(XMLGenerator.createElement("arg", props={"name": "robot_yaw", \
-            "value": "$(eval arg('robot' + str(arg('robot_number')) + '_yaw'))"}))
+        include.append(
+            XMLGenerator.createElement("arg", props={"name": "robot_x", "value": "$(eval arg('robot' + str(arg('robot_number')) + '_x_pos'))"})
+        )
+        include.append(
+            XMLGenerator.createElement("arg", props={"name": "robot_y", "value": "$(eval arg('robot' + str(arg('robot_number')) + '_y_pos'))"})
+        )
+        include.append(
+            XMLGenerator.createElement("arg", props={"name": "robot_z", "value": "$(eval arg('robot' + str(arg('robot_number')) + '_z_pos'))"})
+        )
+        include.append(
+            XMLGenerator.createElement("arg", props={"name": "robot_yaw", "value": "$(eval arg('robot' + str(arg('robot_number')) + '_yaw'))"})
+        )
 
         # recursive start
-        cycle = XMLGenerator.createElement("include", props={"file": "$(find sim_env)/launch/include/robots/start_robots.launch.xml",
-            "if": "$(eval arg('robot_number') > 1)"})
+        cycle = XMLGenerator.createElement(
+            "include", props={"file": "$(find sim_env)/launch/include/robots/start_robots.launch.xml", "if": "$(eval arg('robot_number') > 1)"}
+        )
         cycle.append(XMLGenerator.createElement("arg", props={"name": "robot_number", "value": "$(eval arg('robot_number') - 1)"}))
 
         launch.append(include)
         launch.append(cycle)
         XMLGenerator.indent(launch)
 
-        with open(path, 'wb+') as f:
-            ET.ElementTree(launch).write(f, encoding='utf-8', xml_declaration=True)
+        with open(path, "wb+") as f:
+            ET.ElementTree(launch).write(f, encoding="utf-8", xml_declaration=True)
 
     def writePedestrianWorld(self, path):
-        '''
+        """
         Create configure file to construct pedestrians environment.
 
         Parameters
         ----------
         path: str
             the path of file(.launch.xml) to write.
-        '''
+        """
+
         def createHuman(config, index):
-            '''
+            """
             Create human element of world file.
 
             Parameters
@@ -148,17 +161,18 @@ class XMLGenerator(object):
                 configure data structure.
             index: int
                 human index
-            
+
             Return
             ----------
             human: ET.Element
                 human element ptr
-            '''
+            """
+
             def createCollision(name, scale, pose=None):
                 if pose:
-                    props = {"scale": " ".join('%s' %s for s in scale), "pose": " ".join('%s' %p for p in pose)}
+                    props = {"scale": " ".join("%s" % s for s in scale), "pose": " ".join("%s" % p for p in pose)}
                 else:
-                    props = {"scale": " ".join('%s' %s for s in scale)}
+                    props = {"scale": " ".join("%s" % s for s in scale)}
                 return XMLGenerator.createElement("collision", text=name, props=props)
 
             # configure
@@ -182,8 +196,7 @@ class XMLGenerator(object):
             animation.append(XMLGenerator.createElement("interpolate_x", text="true"))
 
             # plugin
-            plugin = XMLGenerator.createElement("plugin", props={"name": human["name"] + "_plugin", 
-                "filename": "libPedestrianSFMPlugin.so"})
+            plugin = XMLGenerator.createElement("plugin", props={"name": human["name"] + "_plugin", "filename": "libPedestrianSFMPlugin.so"})
             plugin.append(createCollision("LHipJoint_LeftUpLeg_collision", [0.01, 0.001, 0.001]))
             plugin.append(createCollision("LeftUpLeg_LeftLeg_collision", [8.0, 8.0, 1.0]))
             plugin.append(createCollision("LeftLeg_LeftFoot_collision", [8.0, 8.0, 1.0]))
@@ -235,7 +248,6 @@ class XMLGenerator(object):
             XMLGenerator.indent(actor)
 
             return actor
-        
 
         if not "pedestrians" in self.user_cfg or not self.user_cfg["pedestrians"] or not self.user_cfg["world"]:
             return
@@ -249,33 +261,32 @@ class XMLGenerator(object):
         human_num = len(ped_cfg["pedestrians"])
         for i in range(human_num):
             world.append(createHuman(ped_cfg, i))
-        
-        with open(path, 'wb+') as f:
-            tree.write(f, encoding='utf-8', xml_declaration=True)
 
+        with open(path, "wb+") as f:
+            tree.write(f, encoding="utf-8", xml_declaration=True)
 
     @staticmethod
     def yamlParser(path: str) -> dict:
-        '''
+        """
         Parser user configure file(.yaml).
 
         Parameters
         ----------
         path: str
             the path of file(.yaml).
-        
+
         Return
         ----------
         data: dict
             user configuer tree
-        '''
-        with open(path, 'r') as f:
+        """
+        with open(path, "r") as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
         return data
 
     @staticmethod
-    def indent(elem: ET.Element, level: int=0) -> None:
-        '''
+    def indent(elem: ET.Element, level: int = 0, tab_size: int = 2) -> None:
+        """
         Format the generated xml document.
 
         Parameters
@@ -283,11 +294,11 @@ class XMLGenerator(object):
         elem: ET.Element
         level: int
             indent level.
-        '''
-        i = "\n" + level * "\t"
+        """
+        i = "\n" + level * " " * tab_size
         if len(elem):
             if not elem.text or not elem.text.strip():
-                elem.text = i + "\t"
+                elem.text = i + " " * tab_size
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
             for elem in elem:
@@ -299,7 +310,7 @@ class XMLGenerator(object):
                 elem.tail = i
 
     @staticmethod
-    def createElement(name: str, text: str=None, props: dict={}) -> ET.Element:
+    def createElement(name: str, text: str = None, props: dict = {}) -> ET.Element:
         e = ET.Element(name, attrib=props)
         if not text is None:
             e.text = text
@@ -307,6 +318,6 @@ class XMLGenerator(object):
 
 
 xml_gen = XMLGenerator()
-xml_gen.writeEnvMainXml(xml_gen.root_path + 'sim_env/launch/main.launch')
-xml_gen.writeRobotsXml(xml_gen.root_path + 'sim_env/launch/include/robots/start_robots.launch.xml')
-xml_gen.writePedestrianWorld(xml_gen.root_path + 'sim_env/worlds/' + xml_gen.user_cfg["world"] + "_with_pedestrians.world")
+xml_gen.writeEnvMainXml(xml_gen.root_path + "sim_env/launch/main.launch")
+xml_gen.writeRobotsXml(xml_gen.root_path + "sim_env/launch/include/robots/start_robots.launch.xml")
+xml_gen.writePedestrianWorld(xml_gen.root_path + "sim_env/worlds/" + xml_gen.user_cfg["world"] + "_with_pedestrians.world")
