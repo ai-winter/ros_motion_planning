0cf6409ddbe42ed4175b3b965a11575321f75298
Zhanyu Guo <942568052@qq.com>
2024-01-21 16:55:13 +0800

    add orca planner, many todo

---
+++ src/core/local_planner/orca_planner/include/orca_planner.h
@@ -0,0 +1,50 @@
+#ifndef ORCA_PLANNER_H
+#define ORCA_PLANNER_H
+
+// abstract class from which our plugin inherits
+#include <nav_core/base_local_planner.h>
+
+#include <ros/ros.h>
+#include <tf2_ros/buffer.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <boost/bind.hpp>
+
+using namespace std;
+
+namespace orca_planner
+{
+
+class OrcaPlanner : public nav_core::BaseLocalPlanner
+{
+public:
+  OrcaPlanner();
+  OrcaPlanner(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros);
+
+  ~OrcaPlanner();
+
+  void initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros);
+
+  bool setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan);
+
+  bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+
+  bool isGoalReached();
+
+private:
+  costmap_2d::Costmap2DROS* costmap_ros_;
+  tf2_ros::Buffer* tf_;
+  bool initialized_, odom_flag_;
+
+  int agent_number_;
+  int agent_id_;
+  std::vector<ros::Subscriber> odom_subs_;
+  std::vector<nav_msgs::Odometry> other_odoms_;
+
+  void odometryCallback(const nav_msgs::OdometryConstPtr& msg, int agent_id);
+};
+};  // namespace orca_planner
+
+#endif
---
+++ src/core/local_planner/orca_planner/src/orca_planner.cpp
@@ -0,0 +1,83 @@
+#include <pluginlib/class_list_macros.h>
+#include "orca_planner.h"
+
+PLUGINLIB_EXPORT_CLASS(orca_planner::OrcaPlanner, nav_core::BaseLocalPlanner)
+
+namespace orca_planner
+{
+
+OrcaPlanner::OrcaPlanner() : initialized_(false), costmap_ros_(nullptr), tf_(nullptr), odom_flag_(false)
+{
+}
+
+OrcaPlanner::OrcaPlanner(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros) : OrcaPlanner()
+{
+  initialize(name, tf, costmap_ros);
+}
+
+OrcaPlanner::~OrcaPlanner()
+{
+}
+
+// Take note that tf::TransformListener* has been changed to tf2_ros::Buffer* in ROS Noetic
+void OrcaPlanner::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros)
+{
+  if (!initialized_)
+  {
+    initialized_ = true;
+    tf_ = tf;
+    costmap_ros_ = costmap_ros;
+
+    ros::NodeHandle nh = ros::NodeHandle("~/" + name);
+
+    nh.param("agent_number", agent_number_, -1);
+    nh.param("agent_id", agent_id_, -1);
+
+    for (int i = 0; i < agent_number_; ++i)
+    {
+      ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>(
+          "/robot" + std::to_string(i) + "/odom", 1, boost::bind(&OrcaPlanner::odometryCallback, this, _1, i));
+      odom_subs_.push_back(odom_sub);
+      ROS_WARN("agent %d, subscribe to agent %d.", agent_id_, i);
+    }
+  }
+}
+
+void OrcaPlanner::odometryCallback(const nav_msgs::OdometryConstPtr& msg, int agent_id)
+{
+  if (!odom_flag_)
+    odom_flag_ = true;
+
+  other_odoms_[agent_id] = *msg;
+}
+
+bool OrcaPlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan)
+{
+  if (!initialized_)
+  {
+    ROS_ERROR("This planner has not been initialized");
+    return false;
+  }
+  return true;
+}
+
+bool OrcaPlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
+{
+  if (!initialized_)
+  {
+    ROS_ERROR("This planner has not been initialized");
+    return false;
+  }
+  return true;
+}
+
+bool OrcaPlanner::isGoalReached()
+{
+  if (!initialized_)
+  {
+    ROS_ERROR("This planner has not been initialized");
+    return false;
+  }
+  return false;
+}
+}  // namespace orca_planner
