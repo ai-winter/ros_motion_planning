bf229a5a4aebd50482ea5305fbc8a7c3b443ccc2
mr_winter <913982779@qq.com>
2025-04-07 11:41:50 +0800

    fix(glog && gflags bug)

--- 3rd/conanfile.py
+++ 3rd/conanfile.py
@@ -8,6 +8,8 @@ class ExampleRecipe(ConanFile):
 
     def configure(self):
         self.options["osqp"].shared = True
+        self.options["ceres-solver"].shared = True
+        self.options["ceres-solver"].use_glog = True
         self.options["libunwind"].shared = True
         self.options["glog"].shared = True
         self.options["glog"].with_threads = True
@@ -15,6 +17,7 @@ class ExampleRecipe(ConanFile):
 
     def requirements(self):
         self.requires("osqp/0.6.3")
+        self.requires("ceres-solver/1.14.0")
 
     def layout(self):
         cmake_layout(self)
