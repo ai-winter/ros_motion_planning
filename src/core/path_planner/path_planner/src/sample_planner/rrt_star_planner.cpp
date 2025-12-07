221e1826a37b999eeed531e6268f403f9fccb62f
mr_winter <913982779@qq.com>
2025-12-07 16:29:57 +0800

    feat(planner/path_planner): proto-based RRT* path planner and related parameters

--- src/core/path_planner/path_planner/src/sample_planner/rrt_star_planner.cpp
+++ src/core/path_planner/path_planner/src/sample_planner/rrt_star_planner.cpp
@@ -19,27 +19,17 @@
 #include "common/geometry/collision_checker.h"
 #include "path_planner/sample_planner/rrt_star_planner.h"
 
-namespace rmp
-{
-namespace path_planner
-{
-namespace
-{
+using namespace rmp::common::geometry;
 using CollisionChecker = rmp::common::geometry::CollisionChecker;
-}
 
+namespace rmp {
+namespace path_planner {
 /**
  * @brief  Constructor
  * @param   costmap   the environment for path planning
- * @param   obstacle_factor obstacle factor(greater means obstacles)
- * @param   sample_num  andom sample points
- * @param   max_dist    max distance between sample points
- * @param   r           optimization radius
  */
-RRTStarPathPlanner::RRTStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double obstacle_factor, int sample_num,
-                                       double max_dist, double r)
-  : RRTPathPlanner(costmap_ros, obstacle_factor, sample_num, max_dist), r_(r)
-{
+RRTStarPathPlanner::RRTStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros)
+  : RRTPathPlanner(costmap_ros) {
 }
 /**
  * @brief RRT implementation
@@ -48,17 +38,16 @@ RRTStarPathPlanner::RRTStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, do
  * @param expand    containing the node been search during the process
  * @return tuple contatining a bool as to whether a path was found, and the path
  */
-bool RRTStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& path, Points3d& expand)
-{
+bool RRTStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d* path,
+                              Points3d* expand) {
   double m_start_x, m_start_y, m_goal_x, m_goal_y;
   if ((!validityCheck(start.x(), start.y(), m_start_x, m_start_y)) ||
-      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y)))
-  {
+      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y))) {
     return false;
   }
 
-  path.clear();
-  expand.clear();
+  path->clear();
+  expand->clear();
   sample_list_.clear();
 
   // copy
@@ -69,18 +58,18 @@ bool RRTStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3
   goal_.set_y(m_goal_y);
   goal_.set_id(grid2Index(goal_.x(), goal_.y()));
   sample_list_.insert(std::make_pair(start_.id(), start_));
-  expand.emplace_back(m_start_x, m_start_y, 0);
+  expand->emplace_back(m_start_x, m_start_y, 0);
 
   // main loop
   int iteration = 0;
   bool optimized = false;
-  while (iteration < sample_num_)
-  {
+  while (iteration < config_.sample_planner().sample_points()) {
     // generate a random node in the map
     Node sample_node = _generateRandomNode();
 
     // obstacle
-    if (costmap_->getCharMap()[sample_node.id()] >= costmap_2d::LETHAL_OBSTACLE * obstacle_factor_)
+    if (costmap_->getCharMap()[sample_node.id()] >=
+        costmap_2d::LETHAL_OBSTACLE * config_.obstacle_inflation_factor())
       continue;
 
     // visited
@@ -91,23 +80,20 @@ bool RRTStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3
     Node new_node = _findNearestPoint(sample_list_, sample_node);
     if (new_node.id() == -1)
       continue;
-    else
-    {
+    else {
       sample_list_.insert(std::make_pair(new_node.id(), new_node));
-      expand.emplace_back(new_node.x(), new_node.y(), new_node.pid());
+      expand->emplace_back(new_node.x(), new_node.y(), new_node.pid());
     }
 
     // goal found
-    if (_checkGoal(new_node))
-    {
-      path.clear();
+    if (_checkGoal(new_node)) {
+      path->clear();
       const auto& backtrace = _convertClosedListToPath<Node>(sample_list_, start_, goal_);
-      for (auto iter = backtrace.rbegin(); iter != backtrace.rend(); ++iter)
-      {
+      for (auto iter = backtrace.rbegin(); iter != backtrace.rend(); ++iter) {
         // convert to world frame
         double wx, wy;
         costmap_->mapToWorld(iter->x(), iter->y(), wx, wy);
-        path.emplace_back(wx, wy);
+        path->emplace_back(wx, wy);
       }
       optimized = true;
     }
@@ -123,18 +109,17 @@ bool RRTStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3
  * @param node     sample node
  * @return nearest node
  */
-RRTStarPathPlanner::Node RRTStarPathPlanner::_findNearestPoint(std::unordered_map<int, Node>& list, Node& node)
-{
+RRTStarPathPlanner::Node
+RRTStarPathPlanner::_findNearestPoint(std::unordered_map<int, Node>& list, Node& node) {
   Node nearest_node, new_node(node);
   double min_dist = std::numeric_limits<double>::max();
-  for (const auto& p : list)
-  {
+  for (const auto& p : list) {
     // calculate distance
-    double new_dist = std::hypot(p.second.x() - new_node.x(), p.second.y() - new_node.y());
+    double new_dist =
+        std::hypot(p.second.x() - new_node.x(), p.second.y() - new_node.y());
 
     // update nearest node
-    if (new_dist < min_dist)
-    {
+    if (new_dist < min_dist) {
       nearest_node = p.second;
       new_node.set_pid(nearest_node.id());
       new_node.set_g(new_dist + p.second.g());
@@ -143,63 +128,57 @@ RRTStarPathPlanner::Node RRTStarPathPlanner::_findNearestPoint(std::unordered_ma
   }
 
   // distance longer than the threshold
-  if (min_dist > max_dist_)
-  {
+  const double max_dist = config_.sample_planner().sample_max_distance();
+  if (min_dist > max_dist) {
     // connect sample node and nearest node, then move the nearest node
     // forward to sample node with `max_distance` as result
-    double theta = std::atan2(new_node.y() - nearest_node.y(), new_node.x() - nearest_node.x());
-    new_node.set_x(nearest_node.x() + static_cast<int>(max_dist_ * cos(theta)));
-    new_node.set_y(nearest_node.y() + static_cast<int>(max_dist_ * sin(theta)));
+    double theta =
+        std::atan2(new_node.y() - nearest_node.y(), new_node.x() - nearest_node.x());
+    new_node.set_x(nearest_node.x() + static_cast<int>(max_dist * cos(theta)));
+    new_node.set_y(nearest_node.y() + static_cast<int>(max_dist * sin(theta)));
     new_node.set_id(grid2Index(new_node.x(), new_node.y()));
-    new_node.set_g(max_dist_ + nearest_node.g());
+    new_node.set_g(max_dist + nearest_node.g());
   }
 
   // obstacle check
   auto isCollision = [&](const Node& node1, const Node& node2) {
-    return CollisionChecker::BresenhamCollisionDetection(node1, node2, [&](const Node& node) {
-      return costmap_->getCharMap()[grid2Index(node.x(), node.y())] >= costmap_2d::LETHAL_OBSTACLE * obstacle_factor_;
-    });
+    return CollisionChecker::BresenhamCollisionDetection(
+        node1, node2, [&](const Node& node) {
+          return costmap_->getCharMap()[grid2Index(node.x(), node.y())] >=
+                 costmap_2d::LETHAL_OBSTACLE * config_.obstacle_inflation_factor();
+        });
   };
 
-  if (!isCollision(new_node, nearest_node))
-  {
+  if (!isCollision(new_node, nearest_node)) {
     // rewire optimization
-    for (auto& p : sample_list_)
-    {
+    for (auto& p : sample_list_) {
       // inside the optimization circle
-      double new_dist = std::hypot(p.second.x() - new_node.x(), p.second.y() - new_node.y());
-      if (new_dist < r_)
-      {
+      double new_dist =
+          std::hypot(p.second.x() - new_node.x(), p.second.y() - new_node.y());
+      if (new_dist < config_.sample_planner().optimization_radius()) {
         double cost = p.second.g() + new_dist;
         // update new sample node's cost and parent
-        if (new_node.g() > cost)
-        {
-          if (!isCollision(new_node, p.second))
-          {
+        if (new_node.g() > cost) {
+          if (!isCollision(new_node, p.second)) {
             new_node.set_pid(p.second.id());
             new_node.set_g(cost);
           }
-        }
-        else
-        {
+        } else {
           // update nodes' cost inside the radius
           cost = new_node.g() + new_dist;
-          if (cost < p.second.g())
-          {
-            if (!isCollision(new_node, p.second))
-            {
+          if (cost < p.second.g()) {
+            if (!isCollision(new_node, p.second)) {
               p.second.set_pid(new_node.id());
               p.second.set_g(cost);
             }
           }
         }
-      }
-      else
+      } else
         continue;
     }
-  }
-  else
+  } else {
     new_node.set_id(-1);
+  }
   return new_node;
 }
 }  // namespace path_planner
