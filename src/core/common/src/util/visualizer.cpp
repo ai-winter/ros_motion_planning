79b82a281a28ac0e434c2a0e8a750b824043e92a
mr_winter <913982779@qq.com>
2025-12-07 15:49:36 +0800

    fix(common): mismatch of ROS wrapper in visualizer

--- src/core/common/src/util/visualizer.cpp
+++ src/core/common/src/util/visualizer.cpp
@@ -18,57 +18,55 @@
 
 #include "common/util/visualizer.h"
 
-namespace rmp
-{
-namespace common
-{
-namespace util
-{
+namespace rmp {
+namespace common {
+namespace util {
 std_msgs::ColorRGBA Visualizer::RED = Visualizer::_colorInit(1.0, 0.0, 0.0, 1.0);
-std_msgs::ColorRGBA Visualizer::DARK_GREEN = Visualizer::_colorInit(0.43, 0.54, 0.24, 0.5);
+std_msgs::ColorRGBA Visualizer::DARK_GREEN =
+    Visualizer::_colorInit(0.43, 0.54, 0.24, 0.5);
 std_msgs::ColorRGBA Visualizer::PURPLE = Visualizer::_colorInit(1.0, 0.0, 1.0, 1.0);
 
 /**
  * @brief publish lines
  */
-void Visualizer::publishLines2d(const Lines2d& lines, const ros::Publisher& publisher, const std::string& frame_id,
-                                const std::string& ns, std_msgs::ColorRGBA color, double scale)
-{
-  int cnt = 0;
-  visualization_msgs::MarkerArray line_array;
-  for (const auto& line : lines)
-  {
-    visualization_msgs::Marker line_marker;
-    line_marker.header.frame_id = "map";
-    line_marker.header.stamp = ros::Time::now();
-    line_marker.ns = ns + "_" + "line_marker";
-    line_marker.id = cnt;
-    line_marker.type = visualization_msgs::Marker::LINE_LIST;
-    line_marker.action = visualization_msgs::Marker::ADD;
+void Visualizer::publishLines2d(const Lines2d& lines, const ros::Publisher& publisher,
+                                const std::string& frame_id, const std::string& ns,
+                                std_msgs::ColorRGBA color, double scale) {
+  visualization_msgs::Marker line_marker;
+  line_marker.header.frame_id = frame_id;
+  line_marker.header.stamp = ros::Time::now();
+  line_marker.ns = ns;
+  line_marker.id = 0;
+  line_marker.type = visualization_msgs::Marker::LINE_LIST;
+  line_marker.action = visualization_msgs::Marker::ADD;
+  line_marker.lifetime = ros::Duration(0.5);
+  line_marker.pose.orientation.w = 1.0;
+  line_marker.scale.x = scale;
+  line_marker.scale.y = scale;
+  line_marker.scale.z = scale;
+  line_marker.color = color;
+
+  for (const auto& line : lines) {
     geometry_msgs::Point start, end;
+
     start.x = line.first.x();
     start.y = line.first.y();
+    start.z = 0.0;
+
     end.x = line.second.x();
     end.y = line.second.y();
+    end.z = 0.0;
+
     line_marker.points.push_back(start);
     line_marker.points.push_back(end);
-    line_marker.lifetime = ros::Duration(0.5);
-    line_marker.scale.x = scale;
-    line_marker.scale.y = scale;
-    line_marker.scale.z = scale;
-    line_marker.color.r = color.r;
-    line_marker.color.g = color.g;
-    line_marker.color.b = color.b;
-    line_marker.color.a = color.a;
-    cnt++;
-    line_array.markers.push_back(line_marker);
   }
-  publisher.publish(line_array);
-  line_array.markers.clear();
+
+  if (!line_marker.points.empty()) {
+    publisher.publish(line_marker);
+  }
 }
 
-std_msgs::ColorRGBA Visualizer::_colorInit(double r, double g, double b, double a)
-{
+std_msgs::ColorRGBA Visualizer::_colorInit(double r, double g, double b, double a) {
   std_msgs::ColorRGBA color;
   color.r = r;
   color.g = g;
