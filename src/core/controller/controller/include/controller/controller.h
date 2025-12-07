9c2ed202053c1d254cefe1bd566ec8cd1f4487d3
mr_winter <913982779@qq.com>
2025-12-07 14:15:53 +0800

    feat(controller): proto-based controller base class and system configure file

--- src/core/controller/controller/include/controller/controller.h
+++ src/core/controller/controller/include/controller/controller.h
@@ -25,12 +25,12 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PointStamped.h>
 
-namespace rmp
-{
-namespace controller
-{
-class Controller
-{
+#include "common/geometry/point.h"
+#include "system_config/controller_protos/controller.pb.h"
+
+namespace rmp {
+namespace controller {
+class Controller {
 public:
   /**
    * @brief Construct a new Controller object
@@ -43,29 +43,9 @@ public:
   ~Controller();
 
   /**
-   * @brief Set or reset obstacle factor
-   * @param factor  obstacle factor
-   */
-  void setFactor(double factor);
-
-  /**
-   * @brief Set or reset frame name
-   * @param frame_name
-   */
-  void setBaseFrame(std::string base_frame);
-  void setMapFrame(std::string map_frame);
-
-  /**
-   * @brief Regularize angle to [-pi, pi]
-   * @param angle the angle (rad) to regularize
-   * @return reg_angle the regulated angle
-   */
-  double regularizeAngle(double angle);
-
-  /**
    * @brief Get the Yaw Angles from PoseStamped
    * @param ps  PoseStamped to calculate
-   * @return  yaw 
+   * @return  yaw
    */
   double getYawAngle(geometry_msgs::PoseStamped& ps);
 
@@ -75,35 +55,37 @@ public:
    * @param goal  goal pose of robot
    * @return true if robot should perform rotation
    */
-  bool shouldRotateToGoal(const geometry_msgs::PoseStamped& cur, const geometry_msgs::PoseStamped& goal);
+  bool shouldRotateToGoal(const geometry_msgs::PoseStamped& cur,
+                          const geometry_msgs::PoseStamped& goal);
 
   /**
    * @brief Whether to correct the tracking path with rotation operation
    * @param angle_to_path  the angle deviation
    * @return true if robot should perform rotation
    */
-  bool shouldRotateToPath(double angle_to_path, double tolerance = 0.0);
+  bool shouldRotateToPath(double angle_to_path);
 
   /**
    * @brief linear velocity regularization
-   * @param base_odometry odometry of the robot, to get velocity
-   * @param v_d           desired velocity magnitude
-   * @return v            regulated linear velocity
+   * @param v_in  raw linear velocity
+   * @param v_d   desired linear velocity
+   * @return v    regulated linear velocity
    */
-  double linearRegularization(nav_msgs::Odometry& base_odometry, double v_d);
+  double linearRegularization(double v_in, double v_d);
 
   /**
    * @brief angular velocity regularization
-   * @param base_odometry odometry of the robot, to get velocity
-   * @param w_d           desired angular velocity
-   * @return  w           regulated angular velocity
+   * @param w_in  raw angular velocity
+   * @param w_d   desired angular velocity
+   * @return  w   regulated angular velocity
    */
-  double angularRegularization(nav_msgs::Odometry& base_odometry, double w_d);
+  double angularRegularization(double w_in, double w_d);
 
   /**
    * @brief Tranform from in_pose to out_pose with out frame using tf
    */
-  void transformPose(tf2_ros::Buffer* tf, const std::string out_frame, const geometry_msgs::PoseStamped& in_pose,
+  void transformPose(tf2_ros::Buffer* tf, const std::string out_frame,
+                     const geometry_msgs::PoseStamped& in_pose,
                      geometry_msgs::PoseStamped& out_pose) const;
 
   /**
@@ -117,54 +99,34 @@ public:
   bool worldToMap(double wx, double wy, int& mx, int& my);
 
   /**
-   * @brief Prune the path, removing the waypoints that the robot has already passed and distant waypoints
+   * @brief Prune the path, removing the waypoints that the robot has already passed and
+   * distant waypoints
    * @param robot_pose_global the robot's pose  [global]
    * @return pruned path
    */
-  std::vector<geometry_msgs::PoseStamped> prune(const geometry_msgs::PoseStamped robot_pose_global);
+  std::vector<geometry_msgs::PoseStamped>
+  prune(const geometry_msgs::PoseStamped robot_pose_global);
 
   /**
-   * @brief Calculate the look-ahead distance with current speed dynamically
-   * @param vt the current speed
-   * @return L the look-ahead distance
-   */
-  double getLookAheadDistance(double vt);
-
-  /**
-   * @brief find the point on the path that is exactly the lookahead distance away from the robot
+   * @brief find the point on the path that is exactly the lookahead distance away from
+   * the robot
    * @param lookahead_dist    the lookahead distance
    * @param robot_pose_global the robot's pose  [global]
    * @param prune_plan        the pruned plan
    * @param pt                the lookahead point
-   * @param theta             the angle on traj
    * @param kappa             the curvature on traj
    */
-  void getLookAheadPoint(double lookahead_dist, geometry_msgs::PoseStamped robot_pose_global,
-                         const std::vector<geometry_msgs::PoseStamped>& prune_plan, geometry_msgs::PointStamped& pt,
-                         double& theta, double& kappa);
+  void getLookAheadPoint(double lookahead_dist,
+                         geometry_msgs::PoseStamped robot_pose_global,
+                         const std::vector<geometry_msgs::PoseStamped>& prune_plan,
+                         common::geometry::Point3d* pt, double* kappa);
 
 protected:
-  double factor_;  // obstacle factor(greater means obstacles)
-
-  double max_v_, min_v_, max_v_inc_;  // linear velocity
-  double max_w_, min_w_, max_w_inc_;  // angular velocity
-
-  // if the distance is less than the tolerance value, it is considered to have reached the target position
-  double goal_dist_tol_;
-
-  // if the angle deviation is greater than this threshold, perform rotation first
-  double rotate_tol_;
-
-  // frame name of base link, map and odometry
-  std::string base_frame_, map_frame_, odom_frame_;
-
+  pb::controller::Controller config_;
+  double control_dt_;
   std::shared_ptr<base_local_planner::OdometryHelperRos> odom_helper_;  // odometry helper
-  costmap_2d::Costmap2DROS* costmap_ros_;                               // costmap(ROS wrapper)
+  costmap_2d::Costmap2DROS* costmap_ros_;  // costmap(ROS wrapper)
   std::vector<geometry_msgs::PoseStamped> global_plan_;
-
-  double lookahead_time_;      // lookahead time gain
-  double min_lookahead_dist_;  // minimum lookahead distance
-  double max_lookahead_dist_;  // maximum lookahead distance
 };
 }  // namespace controller
 }  // namespace rmp
--- src/core/controller/controller/src/controller.cpp
+++ src/core/controller/controller/src/controller.cpp
@@ -16,61 +16,34 @@
  */
 #include <tf2/utils.h>
 
+#include "common/geometry/angles.h"
+#include "common/geometry/point.h"
 #include "common/math/math_helper.h"
 
 #include "controller/controller.h"
+#include "system_config/system_config.h"
 
-namespace rmp
-{
-namespace controller
-{
+using namespace rmp::common::math;
+using namespace rmp::common::geometry;
+
+namespace rmp {
+namespace controller {
 /**
  * @brief Construct a new Controller object
  */
 Controller::Controller()
-  : factor_(0.5), base_frame_("base_link"), map_frame_("map"), odom_frame_("odom"), costmap_ros_(nullptr)
-{
-  odom_helper_ = std::make_shared<base_local_planner::OdometryHelperRos>(odom_frame_);
+  : config_(system_config::SystemConfigPtr::Instance()->configure().controller())
+  , costmap_ros_(nullptr) {
+  control_dt_ = 1.0 / config_.control_frequency();
+  odom_helper_ =
+      std::make_shared<base_local_planner::OdometryHelperRos>(config_.odom_frame());
 }
 
 /**
  * @brief Destroy the Controller object
  */
-Controller::~Controller()
-{
+Controller::~Controller() {
   // delete odom_helper_;
-}
-
-/**
- * @brief Set or reset obstacle factor
- * @param factor obstacle factor
- */
-void Controller::setFactor(double factor)
-{
-  factor_ = factor;
-}
-
-/**
- * @brief Set or reset frame name
- * @param frame_name
- */
-void Controller::setBaseFrame(std::string base_frame)
-{
-  base_frame_ = base_frame;
-}
-void Controller::setMapFrame(std::string map_frame)
-{
-  map_frame_ = map_frame;
-}
-
-/**
- * @brief Regularize angle to [-pi, pi]
- * @param angle the angle (rad) to regularize
- * @return reg_angle the regulated angle
- */
-double Controller::regularizeAngle(double angle)
-{
-  return angle - 2.0 * M_PI * std::floor((angle + M_PI) / (2.0 * M_PI));
 }
 
 /**
@@ -78,15 +51,9 @@ double Controller::regularizeAngle(double angle)
  * @param ps  PoseStamped to calculate
  * @return  yaw
  */
-double Controller::getYawAngle(geometry_msgs::PoseStamped& ps)
-{
-  tf2::Quaternion q(ps.pose.orientation.x, ps.pose.orientation.y, ps.pose.orientation.z, ps.pose.orientation.w);
-  tf2::Matrix3x3 m(q);
-
-  double roll(0.0), pitch(0.0), yaw(0.0);
-  m.getRPY(roll, pitch, yaw);
-
-  return yaw;
+double Controller::getYawAngle(geometry_msgs::PoseStamped& ps) {
+  return quaternionToHeading(ps.pose.orientation.x, ps.pose.orientation.y,
+                             ps.pose.orientation.z, ps.pose.orientation.w);
 }
 
 /**
@@ -95,10 +62,11 @@ double Controller::getYawAngle(geometry_msgs::PoseStamped& ps)
  * @param goal goal pose of robot
  * @return true if robot should perform rotation
  */
-bool Controller::shouldRotateToGoal(const geometry_msgs::PoseStamped& cur, const geometry_msgs::PoseStamped& goal)
-{
-  return std::hypot(cur.pose.position.x - goal.pose.position.x, cur.pose.position.y - goal.pose.position.y) <
-         goal_dist_tol_;
+bool Controller::shouldRotateToGoal(const geometry_msgs::PoseStamped& cur,
+                                    const geometry_msgs::PoseStamped& goal) {
+  return std::hypot(cur.pose.position.x - goal.pose.position.x,
+                    cur.pose.position.y - goal.pose.position.y) <
+         config_.goal_dist_tolerance();
 }
 
 /**
@@ -106,56 +74,56 @@ bool Controller::shouldRotateToGoal(const geometry_msgs::PoseStamped& cur, const
  * @param angle_to_path the angle deviation
  * @return true if robot should perform rotation
  */
-bool Controller::shouldRotateToPath(double angle_to_path, double tolerance)
-{
-  return (tolerance && (angle_to_path > tolerance)) || (!tolerance && (angle_to_path > rotate_tol_));
+bool Controller::shouldRotateToPath(double angle_to_path) {
+  return angle_to_path > config_.rotate_tolerance();
 }
 
 /**
  * @brief linear velocity regularization
- * @param base_odometry odometry of the robot, to get velocity
- * @param v_d           desired velocity magnitude
- * @return v            regulated linear velocity
+ * @param v_in  raw linear velocity
+ * @param v_d   desired linear velocity
+ * @return v    regulated linear velocity
  */
-double Controller::linearRegularization(nav_msgs::Odometry& base_odometry, double v_d)
-{
-  double v = std::hypot(base_odometry.twist.twist.linear.x, base_odometry.twist.twist.linear.y);
-  double v_inc = v_d - v;
+double Controller::linearRegularization(double v_in, double v_d) {
+  double v_inc = v_d - v_in;
 
-  if (std::fabs(v_inc) > max_v_inc_)
-    v_inc = std::copysign(max_v_inc_, v_inc);
+  if (std::fabs(v_inc) > config_.max_linear_velocity_increment()) {
+    v_inc = std::copysign(config_.max_linear_velocity_increment(), v_inc);
+  }
 
-  double v_cmd = v + v_inc;
-  if (std::fabs(v_cmd) > max_v_)
-    v_cmd = std::copysign(max_v_, v_cmd);
-  else if (std::fabs(v_cmd) < min_v_)
-    v_cmd = std::copysign(min_v_, v_cmd);
+  double v_cmd = v_in + v_inc;
+  if (std::fabs(v_cmd) > config_.max_linear_velocity()) {
+    v_cmd = std::copysign(config_.max_linear_velocity(), v_cmd);
+  } else if (std::fabs(v_cmd) < config_.min_linear_velocity()) {
+    v_cmd = std::copysign(config_.min_linear_velocity(), v_cmd);
+  }
 
   return v_cmd;
 }
 
 /**
  * @brief angular velocity regularization
- * @param base_odometry odometry of the robot, to get velocity
- * @param w_d           desired angular velocity
- * @return w            regulated angular velocity
+ * @param w_in  raw angular velocity
+ * @param w_d   desired angular velocity
+ * @return  w   regulated angular velocity
  */
-double Controller::angularRegularization(nav_msgs::Odometry& base_odometry, double w_d)
-{
-  if (std::fabs(w_d) > max_w_)
-    w_d = std::copysign(max_w_, w_d);
+double Controller::angularRegularization(double w_in, double w_d) {
+  if (std::fabs(w_d) > config_.max_angular_velocity()) {
+    w_d = std::copysign(config_.max_angular_velocity(), w_d);
+  }
 
-  double w = base_odometry.twist.twist.angular.z;
-  double w_inc = w_d - w;
+  double w_inc = w_d - w_in;
 
-  if (std::fabs(w_inc) > max_w_inc_)
-    w_inc = std::copysign(max_w_inc_, w_inc);
+  if (std::fabs(w_inc) > config_.max_angular_velocity_increment()) {
+    w_inc = std::copysign(config_.max_angular_velocity_increment(), w_inc);
+  }
 
-  double w_cmd = w + w_inc;
-  if (std::fabs(w_cmd) > max_w_)
-    w_cmd = std::copysign(max_w_, w_cmd);
-  else if (std::fabs(w_cmd) < min_w_)
-    w_cmd = std::copysign(min_w_, w_cmd);
+  double w_cmd = w_in + w_inc;
+  if (std::fabs(w_cmd) > config_.max_angular_velocity()) {
+    w_cmd = std::copysign(config_.max_angular_velocity(), w_cmd);
+  } else if (std::fabs(w_cmd) < config_.min_angular_velocity()) {
+    w_cmd = std::copysign(config_.min_angular_velocity(), w_cmd);
+  }
 
   return w_cmd;
 }
@@ -164,8 +132,8 @@ double Controller::angularRegularization(nav_msgs::Odometry& base_odometry, doub
  * @brief Tranform from in_pose to out_pose with out frame using tf
  */
 void Controller::transformPose(tf2_ros::Buffer* tf, const std::string out_frame,
-                               const geometry_msgs::PoseStamped& in_pose, geometry_msgs::PoseStamped& out_pose) const
-{
+                               const geometry_msgs::PoseStamped& in_pose,
+                               geometry_msgs::PoseStamped& out_pose) const {
   if (in_pose.header.frame_id == out_frame)
     out_pose = in_pose;
 
@@ -181,8 +149,7 @@ void Controller::transformPose(tf2_ros::Buffer* tf, const std::string out_frame,
  * @param wy world map y
  * @return true if successfull, else false
  */
-bool Controller::worldToMap(double wx, double wy, int& mx, int& my)
-{
+bool Controller::worldToMap(double wx, double wy, int& mx, int& my) {
   unsigned int mx_u, my_u;
   bool flag = costmap_ros_->getCostmap()->worldToMap(wx, wy, mx_u, my_u);
   mx = static_cast<int>(mx_u);
@@ -191,33 +158,40 @@ bool Controller::worldToMap(double wx, double wy, int& mx, int& my)
 }
 
 /**
- * @brief Prune the path, removing the waypoints that the robot has already passed and distant waypoints
+ * @brief Prune the path, removing the waypoints that the robot has already passed and
+ * distant waypoints
  * @param robot_pose_global the robot's pose  [global]
  * @return pruned path
  */
-std::vector<geometry_msgs::PoseStamped> Controller::prune(const geometry_msgs::PoseStamped robot_pose_global)
-{
-  auto calPoseDistance = [](const geometry_msgs::PoseStamped& ps_1, const geometry_msgs::PoseStamped& ps_2) {
-    return std::hypot(ps_1.pose.position.x - ps_2.pose.position.x, ps_1.pose.position.y - ps_2.pose.position.y);
+std::vector<geometry_msgs::PoseStamped>
+Controller::prune(const geometry_msgs::PoseStamped robot_pose_global) {
+  auto calPoseDistance = [](const geometry_msgs::PoseStamped& ps_1,
+                            const geometry_msgs::PoseStamped& ps_2) {
+    return std::hypot(ps_1.pose.position.x - ps_2.pose.position.x,
+                      ps_1.pose.position.y - ps_2.pose.position.y);
   };
 
   /**
-   * @brief Find the first element in iterator that is greater integrated distance than compared value
+   * @brief Find the first element in iterator that is greater integrated distance than
+   * compared value
    * @param begin   The begin of iterator
    * @param end     The end of iterator
    * @param dist    The distance metric function
    * @param cmp_val The compared value
-   * @return it     The first element in iterator that is greater integrated distance than compared value
+   * @return it     The first element in iterator that is greater integrated distance than
+   * compared value
    */
   auto firstIntegratedDistance =
-      [](std::vector<geometry_msgs::PoseStamped>::iterator begin, std::vector<geometry_msgs::PoseStamped>::iterator end,
-         std::function<double(const geometry_msgs::PoseStamped&, const geometry_msgs::PoseStamped&)> dist,
+      [](std::vector<geometry_msgs::PoseStamped>::iterator begin,
+         std::vector<geometry_msgs::PoseStamped>::iterator end,
+         std::function<double(const geometry_msgs::PoseStamped&,
+                              const geometry_msgs::PoseStamped&)>
+             dist,
          double cmp_val) {
         if (begin == end)
           return end;
         double d = 0.0;
-        for (auto it = begin; it != end - 1; it++)
-        {
+        for (auto it = begin; it != end - 1; it++) {
           d += dist(*it, *(it + 1));
           if (d > cmp_val)
             return it + 1;
@@ -240,11 +214,9 @@ std::vector<geometry_msgs::PoseStamped> Controller::prune(const geometry_msgs::P
 
     auto min_val = cal(*begin);
     auto min_iter = begin;
-    for (auto it = ++begin; it != end; it++)
-    {
+    for (auto it = ++begin; it != end; it++) {
       auto val = cal(*it);
-      if (val <= min_val)
-      {
+      if (val <= min_val) {
         min_val = val;
         min_iter = it;
       }
@@ -252,123 +224,103 @@ std::vector<geometry_msgs::PoseStamped> Controller::prune(const geometry_msgs::P
     return min_iter;
   };
 
-  auto closest_pose_upper_bound = firstIntegratedDistance(global_plan_.begin(), global_plan_.end(), calPoseDistance,
-                                                          costmap_ros_->getCostmap()->getSizeInMetersX() / 2.0);
+  auto closest_pose_upper_bound =
+      firstIntegratedDistance(global_plan_.begin(), global_plan_.end(), calPoseDistance,
+                              costmap_ros_->getCostmap()->getSizeInMetersX() / 2.0);
 
   // find the closest pose on the path to the robot
-  auto transform_begin =
-      getMinFuncVal(global_plan_.begin(), closest_pose_upper_bound,
-                    [&](const geometry_msgs::PoseStamped& ps) { return calPoseDistance(robot_pose_global, ps); });
+  auto transform_begin = getMinFuncVal(global_plan_.begin(), closest_pose_upper_bound,
+                                       [&](const geometry_msgs::PoseStamped& ps) {
+                                         return calPoseDistance(robot_pose_global, ps);
+                                       });
 
   // Transform the near part of the global plan into the robot's frame of reference.
   std::vector<geometry_msgs::PoseStamped> prune_path;
   for (auto it = transform_begin; it < global_plan_.end(); it++)
     prune_path.push_back(*it);
 
-  // path pruning: remove the portion of the global plan that already passed so don't process it on the next iteration
+  // path pruning: remove the portion of the global plan that already passed so don't
+  // process it on the next iteration
   global_plan_.erase(std::begin(global_plan_), transform_begin);
 
   return prune_path;
 }
 
 /**
- * @brief Calculate the look-ahead distance with current speed dynamically
- * @param vt the current speed
- * @return L the look-ahead distance
- */
-double Controller::getLookAheadDistance(double vt)
-{
-  double lookahead_dist = fabs(vt) * lookahead_time_;
-  return rmp::common::math::clamp(lookahead_dist, min_lookahead_dist_, max_lookahead_dist_);
-}
-
-/**
- * @brief find the point on the path that is exactly the lookahead distance away from the robot
+ * @brief find the point on the path that is exactly the lookahead distance away from the
+ * robot
  * @param lookahead_dist    the lookahead distance
  * @param robot_pose_global the robot's pose  [global]
  * @param prune_plan        the pruned plan
  * @param pt                the lookahead point
- * @param theta             the angle on traj
  * @param kappa             the curvature on traj
  */
-void Controller::getLookAheadPoint(double lookahead_dist, geometry_msgs::PoseStamped robot_pose_global,
-                                   const std::vector<geometry_msgs::PoseStamped>& prune_plan,
-                                   geometry_msgs::PointStamped& pt, double& theta, double& kappa)
-{
+void Controller::getLookAheadPoint(
+    double lookahead_dist, geometry_msgs::PoseStamped robot_pose_global,
+    const std::vector<geometry_msgs::PoseStamped>& prune_plan, Point3d* pt,
+    double* kappa) {
   double rx = robot_pose_global.pose.position.x;
   double ry = robot_pose_global.pose.position.y;
 
   // Find the first pose which is at a distance greater than the lookahead distance
-  auto goal_pose_it = std::find_if(prune_plan.begin(), prune_plan.end(), [&](const geometry_msgs::PoseStamped& ps) {
-    return std::hypot(ps.pose.position.x - robot_pose_global.pose.position.x,
-                      ps.pose.position.y - robot_pose_global.pose.position.y) >= lookahead_dist;
-  });
+  auto goal_pose_it = std::find_if(
+      prune_plan.begin(), prune_plan.end(), [&](const geometry_msgs::PoseStamped& ps) {
+        return std::hypot(ps.pose.position.x - robot_pose_global.pose.position.x,
+                          ps.pose.position.y - robot_pose_global.pose.position.y) >=
+               lookahead_dist;
+      });
 
   // If the no pose is not far enough, take the last pose
-  if (goal_pose_it == prune_plan.end())
-  {
+  if (goal_pose_it == prune_plan.end()) {
     goal_pose_it = std::prev(prune_plan.end());
-    pt.point.x = goal_pose_it->pose.position.x;
-    pt.point.y = goal_pose_it->pose.position.y;
-    kappa = 0;
-    theta = atan2(pt.point.y - ry, pt.point.x - rx);
-  }
-  else
-  {
+    pt->setX(goal_pose_it->pose.position.x);
+    pt->setY(goal_pose_it->pose.position.y);
+    pt->setTheta(std::atan2(pt->y() - ry, pt->x() - rx));
+    *kappa = 0.0;
+  } else {
     // find the point on the line segment between the two poses
     // that is exactly the lookahead distance away from the robot pose (the origin)
     // This can be found with a closed form for the intersection of a segment and a circle
-    // Because of the way we did the std::find_if, prev_pose is guaranteed to be inside the circle,
-    // and goal_pose is guaranteed to be outside the circle.
+    // Because of the way we did the std::find_if, prev_pose is guaranteed to be inside
+    // the circle, and goal_pose is guaranteed to be outside the circle.
     double px, py;
     double gx = goal_pose_it->pose.position.x;
     double gy = goal_pose_it->pose.position.y;
-    if (goal_pose_it == prune_plan.begin())
-    {
+    if (goal_pose_it == prune_plan.begin()) {
       px = rx;
       py = ry;
-    }
-    else
-    {
+    } else {
       auto prev_pose_it = std::prev(goal_pose_it);
       px = prev_pose_it->pose.position.x;
       py = prev_pose_it->pose.position.y;
     }
 
     // transform to the robot frame so that the circle centers at (0,0)
-    rmp::common::geometry::Vec2d prev_p(px - rx, py - ry);
-    rmp::common::geometry::Vec2d goal_p(gx - rx, gy - ry);
+    common::geometry::Vec2d prev_p(px - rx, py - ry);
+    common::geometry::Vec2d goal_p(gx - rx, gy - ry);
     std::vector<rmp::common::geometry::Vec2d> i_points =
-        rmp::common::math::circleSegmentIntersection(prev_p, goal_p, lookahead_dist);
+        circleSegmentIntersection(prev_p, goal_p, lookahead_dist);
 
     double dist_to_goal = std::numeric_limits<double>::max();
-    for (const auto& i_point : i_points)
-    {
+    for (const auto& i_point : i_points) {
       double dist = std::hypot(i_point.x() + rx - gx, i_point.y() + ry - gy);
-      if (dist < dist_to_goal)
-      {
+      if (dist < dist_to_goal) {
         dist_to_goal = dist;
-        pt.point.x = i_point.x() + rx;
-        pt.point.y = i_point.y() + ry;
+        pt->setX(i_point.x() + rx);
+        pt->setY(i_point.y() + ry);
       }
     }
 
     auto next_pose_it = std::next(goal_pose_it);
-    if (next_pose_it != prune_plan.end())
-    {
-      rmp::common::geometry::Vec2d p1(px, py), p2(gx, gy),
+    if (next_pose_it != prune_plan.end()) {
+      common::geometry::Vec2d p1(px, py), p2(gx, gy),
           p3(next_pose_it->pose.position.x, next_pose_it->pose.position.y);
-      kappa = rmp::common::math::arcCenter(p1, p2, p3, false);
+      *kappa = arcCenter(p1, p2, p3, false);
+    } else {
+      *kappa = 0.0;
     }
-    else
-    {
-      kappa = 0.0;
-    }
-    theta = atan2(gy - py, gx - px);
+    pt->setTheta(std::atan2(gy - py, gx - px));
   }
-
-  pt.header.frame_id = goal_pose_it->header.frame_id;
-  pt.header.stamp = goal_pose_it->header.stamp;
 }
 
 }  // namespace controller
