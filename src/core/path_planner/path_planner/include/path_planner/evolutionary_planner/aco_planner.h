31ef017a4707df3e08d5e16dbd3c534b680fdcf2
mr_winter <913982779@qq.com>
2025-12-07 16:33:12 +0800

    feat(planner/path_planner): proto-based ACO path planner and related parameters

--- src/core/path_planner/path_planner/include/path_planner/evolutionary_planner/aco_planner.h
+++ src/core/path_planner/path_planner/include/path_planner/evolutionary_planner/aco_planner.h
@@ -23,33 +23,32 @@
 
 #include "path_planner/path_planner.h"
 #include "common/geometry/curve/bspline_curve.h"
+#include "system_config/path_planner_protos/evolutionary_planner/aco_planner.pb.h"
 
-namespace rmp
-{
-namespace path_planner
-{
+namespace rmp {
+namespace path_planner {
 /**
  * @brief Class for objects that plan using the GA algorithm
  */
 
-class ACOPathPlanner : public PathPlanner
-{
+class ACOPathPlanner : public PathPlanner {
 private:
-  using PositionSequence = std::vector<Points2d>;
+  using PositionSequence = std::vector<common::geometry::Points2d>;
 
 protected:
-  struct Ant
-  {
-    Points2d position;    // genets position
-    double fitness;       // genets fitness
-    Points2d best_pos;    // Personal best position in iteration
-    double best_fitness;  // Personal best fitness in iteration
+  struct Ant {
+    common::geometry::Points2d position;  // genets position
+    double fitness;                       // genets fitness
+    common::geometry::Points2d best_pos;  // Personal best position in iteration
+    double best_fitness;                  // Personal best fitness in iteration
 
     Ant() = default;
 
-    Ant(const Points2d& initial_position, double initial_fitness)
-      : position(initial_position), fitness(initial_fitness), best_pos(initial_position), best_fitness(initial_fitness)
-    {
+    Ant(const common::geometry::Points2d& initial_position, double initial_fitness)
+      : position(initial_position)
+      , fitness(initial_fitness)
+      , best_pos(initial_position)
+      , best_fitness(initial_fitness) {
     }
   };
 
@@ -57,44 +56,37 @@ public:
   /**
    * @brief Construct a new ACO object
    * @param costmap   the environment for path planning
-   * @param obstacle_factor obstacle factor(greater means obstacles)
-   * @param n_ants			number of ants
-   * @param alpha				pheromone weight coefficient
-   * @param beta				heuristic factor weight coefficient
-   * @param rho					evaporation coefficient
-   * @param Q						pheromone gain
-   * @param max_iter		maximum iterations
    */
-  ACOPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double obstacle_factor, int n_ants, int n_inherited,
-                 int point_num, double alpha, double beta, double rho, double Q, int init_mode, int max_iter);
+  ACOPathPlanner(costmap_2d::Costmap2DROS* costmap_ros);
   ~ACOPathPlanner();
 
   /**
    * @brief ACO implementation
    * @param start         start node
    * @param goal          goal node
-   * @param path          optimal path consists of Node
+   * @param path          The resulting path in (x, y, theta)
    * @param expand        containing the node been search during the process
    * @return  true if path found, else false
    */
-  bool plan(const Point3d& start, const Point3d& goal, Points3d& path, Points3d& expand);
+  bool plan(const common::geometry::Point3d& start, const common::geometry::Point3d& goal,
+            common::geometry::Points3d* path, common::geometry::Points3d* expand);
 
   /**
    * @brief Generate n ants with pointNum_ positions each within the map range
    * @param initial_positions The initial position sequence of ants
    * @param start     start node
    * @param goal      goal node
-   * @param gen_mode  generation mode
    */
-  void initializePositions(PositionSequence& initial_positions, const Point3d& start, const Point3d& goal,
-                           int gen_mode = GEN_MODE::CIRCLE);
+  void initializePositions(PositionSequence& initial_positions,
+                           const common::geometry::Point3d& start,
+                           const common::geometry::Point3d& goal);
 
   /**
    * @brief Calculate the value of fitness function
    * @param position  the control points calculated by PSO
    * @return fitness the value of fitness function
    */
-  double calFitnessValue(const Points2d& position);
+  double calFitnessValue(const common::geometry::Points2d& position);
 
   /**
    * @brief Ant update optimization iteration
@@ -103,33 +95,22 @@ public:
    * @param gen            randomizer
    * @param expand         containing the node been search during the process
    */
-  void optimizeAnt(Ant& ant, Ant& best_ant, std::mt19937& gen, Points3d& expand);
+  void optimizeAnt(Ant& ant, Ant& best_ant, std::mt19937& gen,
+                   common::geometry::Points3d* expand);
 
-  void updateAnts(std::vector<Ant>& ants, const Point3d& start, const Point3d& goal, Ant& best_ant);
-
-public:
-  enum GEN_MODE
-  {
-    RANDOM = 1,
-    CIRCLE = 2,
-  };
+  void updateAnts(std::vector<Ant>& ants, const common::geometry::Point3d& start,
+                  const common::geometry::Point3d& goal, Ant& best_ant);
 
 protected:
-  int n_ants_;             // number of ants
-  int n_inherited_;        // number of inherited ants
-  int point_num_;          // number of position points contained in each ant
-  double alpha_, beta_;    // pheromone and heuristic factor weight coefficient
-  double rho_;             // evaporation coefficient
-  double Q_;               // pheromone gain
-  int init_mode_;          // Set the generation mode for the initial position points of the genets swarm
-  int max_iter_;           // maximum iterations
-  Point3d start_, goal_;   // paired start and goal point for path smoothing
+  // paired start and goal point for path smoothing
+  common::geometry::Point3d start_, goal_;
   double* pheromone_mat_;  // pheromone matrix
 
 private:
-  std::mutex mutex_;                                                  // thread lock
-  std::vector<Ant> inherited_ants_;                                   // inherited ants
-  std::unique_ptr<rmp::common::geometry::BSplineCurve> bspline_gen_;  // Path generation
+  pb::path_planner::ACOPlanner aco_config_;
+  std::mutex mutex_;                                             // thread lock
+  std::vector<Ant> inherited_ants_;                              // inherited ants
+  std::unique_ptr<common::geometry::BSplineCurve> bspline_gen_;  // Path generation
 };
 
 }  // namespace path_planner
--- src/core/path_planner/path_planner/src/evolutionary_planner/aco_planner.cpp
+++ src/core/path_planner/path_planner/src/evolutionary_planner/aco_planner.cpp
@@ -21,42 +21,21 @@
 #include "common/math/math_helper.h"
 #include "path_planner/evolutionary_planner/aco_planner.h"
 
-namespace rmp
-{
-namespace path_planner
-{
+using namespace rmp::common::geometry;
+namespace rmp {
+namespace path_planner {
 /**
  * @brief Construct a new ACO object
  * @param costmap   the environment for path planning
- * @param obstacle_factor obstacle factor(greater means obstacles)
- * @param n_ants			number of ants
- * @param alpha				pheromone weight coefficient
- * @param beta				heuristic factor weight coefficient
- * @param rho					evaporation coefficient
- * @param Q						pheromone gain
- * @param max_iter		maximum iterations
  */
-ACOPathPlanner::ACOPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double obstacle_factor, int n_ants,
-                               int n_inherited, int point_num, double alpha, double beta, double rho, double Q,
-                               int init_mode, int max_iter)
-  : PathPlanner(costmap_ros, obstacle_factor)
-  , n_ants_(n_ants)
-  , n_inherited_(n_inherited)
-  , point_num_(point_num)
-  , alpha_(alpha)
-  , beta_(beta)
-  , rho_(rho)
-  , Q_(Q)
-  , init_mode_(init_mode)
-  , max_iter_(max_iter)
-{
-  bspline_gen_ = std::make_unique<rmp::common::geometry::BSplineCurve>();
-  inherited_ants_.emplace_back(Points2d(point_num), 0.0);
+ACOPathPlanner::ACOPathPlanner(costmap_2d::Costmap2DROS* costmap_ros)
+  : PathPlanner(costmap_ros), bspline_gen_(std::make_unique<BSplineCurve>()) {
+  aco_config_ = config_.evolutionary_planner().aco_planner();
+  inherited_ants_.emplace_back(Points2d(aco_config_.control_point_num()), 0.0);
   pheromone_mat_ = new double[map_size_];
 }
 
-ACOPathPlanner::~ACOPathPlanner()
-{
+ACOPathPlanner::~ACOPathPlanner() {
   delete[] pheromone_mat_;
 }
 
@@ -64,16 +43,15 @@ ACOPathPlanner::~ACOPathPlanner()
  * @brief ACO implementation
  * @param start         start node
  * @param goal          goal node
- * @param path          optimal path consists of Node
+ * @param path          The resulting path in (x, y, theta)
  * @param expand        containing the node been search during the process(unused)
  * @return  true if path found, else false
  */
-bool ACOPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& path, Points3d& expand)
-{
+bool ACOPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d* path,
+                          Points3d* expand) {
   double m_start_x, m_start_y, m_goal_x, m_goal_y;
   if ((!validityCheck(start.x(), start.y(), m_start_x, m_start_y)) ||
-      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y)))
-  {
+      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y))) {
     return false;
   }
 
@@ -83,14 +61,15 @@ bool ACOPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& p
   goal_.setX(m_goal_x);
   goal_.setY(m_goal_y);
   goal_.setTheta(goal.theta());
-  expand.clear();
-  for (int i = 0; i < map_size_; i++)
+  expand->clear();
+  for (int i = 0; i < map_size_; i++) {
     pheromone_mat_[i] = 1.0;
+  }
 
   // variable initialization
   Ant best_ant;
   best_ant.fitness = -1;
-  std::vector<Ant> ants(n_ants_);
+  std::vector<Ant> ants(aco_config_.population_num());
 
   updateAnts(ants, start_, goal_, best_ant);
 
@@ -99,28 +78,30 @@ bool ACOPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& p
   std::mt19937 gen(rd());
 
   // Iterative optimization
-  for (int iter = 0; iter < max_iter_; iter++)
-  {
+  for (int iter = 0; iter < aco_config_.max_iterations(); iter++) {
     updateAnts(ants, start_, goal_, best_ant);
-    std::vector<std::thread> ants_list = std::vector<std::thread>(n_ants_);
-    for (int i = 0; i < n_ants_; ++i)
-      ants_list[i] = std::thread(&ACOPathPlanner::optimizeAnt, this, std::ref(ants[i]), std::ref(best_ant),
-                                 std::ref(gen), std::ref(expand));
-    for (int i = 0; i < n_ants_; ++i)
+    std::vector<std::thread> ants_list =
+        std::vector<std::thread>(aco_config_.population_num());
+    for (int i = 0; i < aco_config_.population_num(); ++i) {
+      ants_list[i] = std::thread(&ACOPathPlanner::optimizeAnt, this, std::ref(ants[i]),
+                                 std::ref(best_ant), std::ref(gen), std::ref(expand));
+    }
+    for (int i = 0; i < aco_config_.population_num(); ++i) {
       ants_list[i].join();
+    }
 
     // best_ant.position = ants[best_ant_idx_].best_pos;
 
     // pheromone deterioration
-    for (int i = 0; i < map_size_; i++)
-      pheromone_mat_[i] *= (1 - rho_);
+    for (int i = 0; i < map_size_; i++) {
+      pheromone_mat_[i] *= (1 - aco_config_.rho());
+    }
   }
 
   // Generating Paths from Optimal Particles
   Points3d points, b_path;
   points.emplace_back(m_start_x, m_start_y, start.theta());
-  for (const auto& pos : best_ant.position)
-  {
+  for (const auto& pos : best_ant.position) {
     points.emplace_back(pos.x(), pos.y());
   }
   points.emplace_back(m_goal_x, m_goal_y, goal.theta());
@@ -129,23 +110,24 @@ bool ACOPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& p
   bspline_gen_->run(points, b_path);
 
   // Path data structure conversion
-  path.clear();
-  for (const auto& pt : b_path)
-  {
+  path->clear();
+  for (const auto& pt : b_path) {
     // convert to world frame
     double wx, wy;
     costmap_->mapToWorld(pt.x(), pt.y(), wx, wy);
-    path.emplace_back(wx, wy, pt.theta());
+    path->emplace_back(wx, wy, pt.theta());
   }
 
   // Update inheritance ants based on optimal fitness
-  std::sort(ants.begin(), ants.end(), [](const Ant& a, const Ant& b) { return a.best_fitness > b.best_fitness; });
+  std::sort(ants.begin(), ants.end(),
+            [](const Ant& a, const Ant& b) { return a.best_fitness > b.best_fitness; });
   inherited_ants_.clear();
 
-  for (int inherit = 0; inherit < n_inherited_; ++inherit)
+  for (int inherit = 0; inherit < aco_config_.inherited_num(); ++inherit) {
     inherited_ants_.emplace_back(ants[inherit]);
+  }
 
-  return !path.empty();
+  return !path->empty();
 }
 
 /**
@@ -153,15 +135,13 @@ bool ACOPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& p
  * @param initial_positions The initial position sequence of ants
  * @param start     start node
  * @param goal      goal node
- * @param gen_mode  generation mode
  */
-void ACOPathPlanner::initializePositions(PositionSequence& initial_positions, const Point3d& start, const Point3d& goal,
-                                         int gen_mode)
-{
+void ACOPathPlanner::initializePositions(PositionSequence& initial_positions,
+                                         const Point3d& start, const Point3d& goal) {
   // Use a random device and engine to generate random numbers
   std::random_device rd;
   std::mt19937 gen(rd());
-  int x[point_num_], y[point_num_];
+  int x[aco_config_.control_point_num()], y[aco_config_.control_point_num()];
   int point_id, pos_id;
 
   // Calculate sequence direction
@@ -171,8 +151,8 @@ void ACOPathPlanner::initializePositions(PositionSequence& initial_positions, co
   // circle generation
   int center_x, center_y;
   double radius;
-  if (gen_mode == GEN_MODE::CIRCLE)
-  {
+  if (config_.evolutionary_planner().init_mode() ==
+      pb::path_planner::EvolutionaryInitMode::EVOLUTIONARY_INIT_MODE_RANDOM_IN_CIRCLE) {
     // Calculate the center and the radius of the circle (midpoint between start and goal)
     center_x = (start.x() + goal.x()) / 2;
     center_y = (start.y() + goal.y()) / 2;
@@ -181,8 +161,7 @@ void ACOPathPlanner::initializePositions(PositionSequence& initial_positions, co
   }
 
   // initialize n_particles positions
-  for (int i = 0; i < n_ants_; ++i)
-  {
+  for (int i = 0; i < aco_config_.population_num(); ++i) {
     Points2d ant_positions;
     std::unordered_set<int> visited;
     std::vector<double> probabilities;
@@ -190,18 +169,16 @@ void ACOPathPlanner::initializePositions(PositionSequence& initial_positions, co
     point_id = 0;
 
     // Generate k * point_num_ unique coordinates
-    int temp_size = 3 * point_num_;
+    int temp_size = 3 * aco_config_.population_num();
     int temp_x[temp_size], temp_y[temp_size];
-    while (point_id < temp_size)
-    {
-      if (gen_mode == GEN_MODE::RANDOM)
-      {
+    while (point_id < temp_size) {
+      if (config_.evolutionary_planner().init_mode() ==
+          pb::path_planner::EvolutionaryInitMode::
+              EVOLUTIONARY_INIT_MODE_RANDOM_IN_MAP_UNSPECIFIED) {
         temp_x[point_id] = std::uniform_int_distribution<int>(0, nx_ - 1)(gen);
         temp_y[point_id] = std::uniform_int_distribution<int>(0, ny_ - 1)(gen);
         pos_id = grid2Index(temp_x[point_id], temp_y[point_id]);
-      }
-      else
-      {
+      } else {
         // Generate random angle in radians
         double angle = std::uniform_real_distribution<double>(0, 2 * M_PI)(gen);
         // Generate random distance from the center within the circle
@@ -210,49 +187,58 @@ void ACOPathPlanner::initializePositions(PositionSequence& initial_positions, co
         temp_x[point_id] = static_cast<int>(std::round(center_x + r * std::cos(angle)));
         temp_y[point_id] = static_cast<int>(std::round(center_y + r * std::sin(angle)));
         // Check if the coordinates are within the map range
-        if (temp_x[point_id] >= 0 && temp_x[point_id] < nx_ && temp_y[point_id] >= 0 && temp_y[point_id] < ny_)
+        if (temp_x[point_id] >= 0 && temp_x[point_id] < nx_ && temp_y[point_id] >= 0 &&
+            temp_y[point_id] < ny_) {
           pos_id = grid2Index(temp_x[point_id], temp_y[point_id]);
-        else
+        } else {
           continue;
+        }
       }
 
       // Check if the coordinates have already been used
-      if (visited.find(pos_id) == visited.end())
-      {
+      if (visited.find(pos_id) == visited.end()) {
         point_id++;
         visited.insert(pos_id);
-        double prob_new = std::pow(pheromone_mat_[pos_id], alpha_) *
-                          std::pow(1.0 / hypot(temp_x[point_id] - goal.x(), temp_y[point_id] - goal.y()), beta_);
+        double prob_new = std::pow(pheromone_mat_[pos_id], aco_config_.alpha()) *
+                          std::pow(1.0 / hypot(temp_x[point_id] - goal.x(),
+                                               temp_y[point_id] - goal.y()),
+                                   aco_config_.beta());
         probabilities.push_back(prob_new);
         prob_sum += prob_new;
       }
     }
 
     // roulette selection
-    std::for_each(probabilities.begin(), probabilities.end(), [&](double& p) { p /= prob_sum; });
+    std::for_each(probabilities.begin(), probabilities.end(),
+                  [&](double& p) { p /= prob_sum; });
     std::random_device device;
     std::mt19937 engine(device());
     std::discrete_distribution<> dist(probabilities.begin(), probabilities.end());
-    for (int j = 0; j < point_num_; j++)
-    {
+    for (int j = 0; j < aco_config_.control_point_num(); j++) {
       int idx = dist(engine);
       x[j] = temp_x[idx];
       y[j] = temp_y[idx];
     }
 
     // sort
-    if (x_order)
-      std::sort(x, x + point_num_, [](int a, int b) { return a < b; });
-    else
-      std::sort(x, x + point_num_, [](int a, int b) { return a > b; });
-    if (y_order)
-      std::sort(y, y + point_num_, [](int a, int b) { return a < b; });
-    else
-      std::sort(y, y + point_num_, [](int a, int b) { return a > b; });
-
+    if (x_order) {
+      std::sort(x, x + aco_config_.control_point_num(),
+                [](int a, int b) { return a < b; });
+    } else {
+      std::sort(x, x + aco_config_.control_point_num(),
+                [](int a, int b) { return a > b; });
+    }
+    if (y_order) {
+      std::sort(y, y + aco_config_.control_point_num(),
+                [](int a, int b) { return a < b; });
+    } else {
+      std::sort(y, y + aco_config_.control_point_num(),
+                [](int a, int b) { return a > b; });
+    }
     // Store elements from x and y in particle_positions
-    for (int j = 0; j < point_num_; ++j)
+    for (int j = 0; j < aco_config_.control_point_num(); ++j) {
       ant_positions.emplace_back(x[j], y[j]);
+    }
 
     initial_positions.push_back(ant_positions);
   }
@@ -263,12 +249,10 @@ void ACOPathPlanner::initializePositions(PositionSequence& initial_positions, co
  * @param position  the control points calculated by PSO
  * @return fitness the value of fitness function
  */
-double ACOPathPlanner::calFitnessValue(const Points2d& position)
-{
+double ACOPathPlanner::calFitnessValue(const Points2d& position) {
   Points3d points, b_path;
   points.emplace_back(start_.x(), start_.y(), start_.theta());
-  for (const auto& pos : position)
-  {
+  for (const auto& pos : position) {
     points.emplace_back(pos.x(), pos.y());
   }
   points.emplace_back(goal_.x(), goal_.y(), goal_.theta());
@@ -279,12 +263,13 @@ double ACOPathPlanner::calFitnessValue(const Points2d& position)
   // collision detection
   int point_index;
   double obs_cost = 1;
-  for (size_t i = 1; i < b_path.size(); ++i)
-  {
-    point_index = grid2Index(static_cast<int>(b_path[i].x()), static_cast<int>(b_path[i].y()));
+  for (size_t i = 1; i < b_path.size(); ++i) {
+    point_index =
+        grid2Index(static_cast<int>(b_path[i].x()), static_cast<int>(b_path[i].y()));
     // next node hit the boundary or obstacle
     if ((point_index < 0) || (point_index >= map_size_) ||
-        (costmap_->getCharMap()[point_index] >= costmap_2d::LETHAL_OBSTACLE * obstacle_factor_))
+        (costmap_->getCharMap()[point_index] >=
+         costmap_2d::LETHAL_OBSTACLE * config_.obstacle_inflation_factor()))
       obs_cost++;
   }
   // Calculate particle fitness
@@ -299,50 +284,48 @@ double ACOPathPlanner::calFitnessValue(const Points2d& position)
  * @param gen            randomizer
  * @param expand         containing the node been search during the process
  */
-void ACOPathPlanner::optimizeAnt(Ant& ant, Ant& best_ant, std::mt19937& gen, Points3d& expand)
-{
+void ACOPathPlanner::optimizeAnt(Ant& ant, Ant& best_ant, std::mt19937& gen,
+                                 Points3d* expand) {
   // Calculate fitness
   ant.fitness = calFitnessValue(ant.position);
 
   // reward here, increased pheromone
-  double c = Q_ / static_cast<double>(ant.fitness);
+  double c = aco_config_.q() / static_cast<double>(ant.fitness);
 
   // Update expand points
   std::lock_guard<std::mutex> lock(mutex_);
-  for (const auto& pos : ant.position)
-  {
+  for (const auto& pos : ant.position) {
     pheromone_mat_[grid2Index(static_cast<int>(pos.x()), static_cast<int>(pos.y()))] += c;
-    expand.emplace_back(pos.x(), pos.y());
+    expand->emplace_back(pos.x(), pos.y());
   }
 
-  if (ant.best_fitness > best_ant.fitness)
-  {
+  if (ant.best_fitness > best_ant.fitness) {
     best_ant.fitness = ant.best_fitness;
     best_ant.position = ant.position;
   }
 }
 
-void ACOPathPlanner::updateAnts(std::vector<Ant>& ants, const Point3d& start, const Point3d& goal, Ant& best_ant)
-{
+void ACOPathPlanner::updateAnts(std::vector<Ant>& ants, const Point3d& start,
+                                const Point3d& goal, Ant& best_ant) {
   double init_fitness;
   PositionSequence init_positions;
 
   // Generate initial position of particle swarm
-  initializePositions(init_positions, start, goal, init_mode_);
+  initializePositions(init_positions, start, goal);
 
   // Ant initialization
-  for (int i = 0; i < n_ants_; ++i)
-  {
+  for (int i = 0; i < aco_config_.population_num(); ++i) {
     Points2d init_position;
-    if ((i < n_inherited_) && (static_cast<int>(inherited_ants_.size()) == n_inherited_))
+    if ((i < aco_config_.inherited_num()) &&
+        (static_cast<int>(inherited_ants_.size()) == aco_config_.inherited_num())) {
       init_position = inherited_ants_[i].best_pos;
-    else
+    } else {
       init_position = init_positions[i];
+    }
 
     // Calculate fitness
     init_fitness = calFitnessValue(init_position);
-    if (init_fitness > best_ant.fitness)
-    {
+    if (init_fitness > best_ant.fitness) {
       best_ant.position = init_position;
       best_ant.fitness = init_fitness;
     }
