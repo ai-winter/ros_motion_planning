6a67c2324160a2406c380fdfb7c06c5b7377b43a
mr_winter <913982779@qq.com>
2025-12-07 15:57:52 +0800

    feat(planner/path_planner): proto-based A* path planner

--- src/core/path_planner/path_planner/include/path_planner/graph_planner/astar_planner.h
+++ src/core/path_planner/path_planner/include/path_planner/graph_planner/astar_planner.h
@@ -1,7 +1,7 @@
 /**
  * *********************************************************
  *
- * @file: a_star.h
+ * @file: astar_planner.h
  * @brief: Contains the A* (dijkstra and GBFS) planner class
  * @author: Yang Haodong
  * @date: 2023-12-12
@@ -19,45 +19,39 @@
 
 #include "path_planner/path_planner.h"
 
-namespace rmp
-{
-namespace path_planner
-{
+namespace rmp {
+namespace path_planner {
 /**
  * @brief Class for objects that plan using the A* algorithm
  */
-class AStarPathPlanner : public PathPlanner
-{
+class AStarPathPlanner : public PathPlanner {
 public:
   /**
    * @brief Construct a new AStar object
    * @param costmap   the environment for path planning
-   * @param obstacle_factor obstacle factor(greater means obstacles)
    * @param dijkstra   using diksktra implementation
    * @param gbfs       using gbfs implementation
    */
-  AStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double obstacle_factor = 1.0, bool dijkstra = false,
+  AStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, bool dijkstra = false,
                    bool gbfs = false);
 
   /**
    * @brief A* implementation
    * @param start          start node
    * @param goal           goal node
-   * @param path           optimal path consists of Node
+   * @param path           The resulting path in (x, y, theta)
    * @param expand         containing the node been search during the process
    * @return true if path found, else false
    */
-  bool plan(const Point3d& start, const Point3d& goal, Points3d& path, Points3d& expand);
+  bool plan(const common::geometry::Point3d& start, const common::geometry::Point3d& goal,
+            common::geometry::Points3d* path, common::geometry::Points3d* expand);
 
 private:
   bool is_dijkstra_;  // using diksktra
   bool is_gbfs_;      // using greedy best first search(GBFS)
 
   using Node = rmp::common::structure::Node<int>;
-  const std::vector<Node> motions = {
-    { 0, 1, 1.0 },          { 1, 0, 1.0 },           { 0, -1, 1.0 },          { -1, 0, 1.0 },
-    { 1, 1, std::sqrt(2) }, { 1, -1, std::sqrt(2) }, { -1, 1, std::sqrt(2) }, { -1, -1, std::sqrt(2) },
-  };
+  static std::vector<Node> motions_;
 };
 }  // namespace path_planner
 }  // namespace rmp
--- src/core/path_planner/path_planner/src/graph_planner/astar_planner.cpp
+++ src/core/path_planner/path_planner/src/graph_planner/astar_planner.cpp
@@ -23,28 +23,31 @@
 #include "common/structure/node.h"
 #include "path_planner/graph_planner/astar_planner.h"
 
-namespace rmp
-{
-namespace path_planner
-{
+using namespace rmp::common::geometry;
+
+namespace rmp {
+namespace path_planner {
+std::vector<AStarPathPlanner::Node> AStarPathPlanner::motions_ = {
+  { 0, 1, 1.0 },           { 1, 0, 1.0 },
+  { 0, -1, 1.0 },          { -1, 0, 1.0 },
+  { 1, 1, std::sqrt(2) },  { 1, -1, std::sqrt(2) },
+  { -1, 1, std::sqrt(2) }, { -1, -1, std::sqrt(2) },
+};
+
 /**
  * @brief Construct a new AStar object
  * @param costmap   the environment for path planning
  * @param dijkstra   using diksktra implementation
  * @param gbfs       using gbfs implementation
  */
-AStarPathPlanner::AStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double obstacle_factor, bool dijkstra,
+AStarPathPlanner::AStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, bool dijkstra,
                                    bool gbfs)
-  : PathPlanner(costmap_ros, obstacle_factor)
-{
+  : PathPlanner(costmap_ros) {
   // can not using both dijkstra and GBFS at the same time
-  if (!(dijkstra && gbfs))
-  {
+  if (!(dijkstra && gbfs)) {
     is_dijkstra_ = dijkstra;
     is_gbfs_ = gbfs;
-  }
-  else
-  {
+  } else {
     is_dijkstra_ = false;
     is_gbfs_ = false;
   }
@@ -54,16 +57,15 @@ AStarPathPlanner::AStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double
  * @brief A* implementation
  * @param start          start node
  * @param goal           goal node
- * @param path           optimal path consists of Node
+ * @param path           The resulting path in (x, y, theta)
  * @param expand         containing the node been search during the process
  * @return true if path found, else false
  */
-bool AStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& path, Points3d& expand)
-{
+bool AStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d* path,
+                            Points3d* expand) {
   double m_start_x, m_start_y, m_goal_x, m_goal_y;
   if ((!validityCheck(start.x(), start.y(), m_start_x, m_start_y)) ||
-      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y)))
-  {
+      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y))) {
     return false;
   }
 
@@ -73,8 +75,8 @@ bool AStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d&
   goal_node.set_id(grid2Index(goal_node.x(), goal_node.y()));
 
   // clear vector
-  path.clear();
-  expand.clear();
+  path->clear();
+  expand->clear();
 
   // open list and closed list
   std::priority_queue<Node, std::vector<Node>, Node::compare_cost> open_list;
@@ -83,8 +85,7 @@ bool AStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d&
   open_list.push(start_node);
 
   // main process
-  while (!open_list.empty())
-  {
+  while (!open_list.empty()) {
     // pop current node from open list
     auto current = open_list.top();
     open_list.pop();
@@ -94,50 +95,55 @@ bool AStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d&
       continue;
 
     closed_list.insert(std::make_pair(current.id(), current));
-    expand.emplace_back(current.x(), current.y());
+    expand->emplace_back(current.x(), current.y());
 
     // goal found
-    if (current == goal_node)
-    {
-      const auto& backtrace = _convertClosedListToPath<Node>(closed_list, start_node, goal_node);
-      for (auto iter = backtrace.rbegin(); iter != backtrace.rend(); ++iter)
-      {
+    if (current == goal_node) {
+      const auto& backtrace =
+          _convertClosedListToPath<Node>(closed_list, start_node, goal_node);
+      for (auto iter = backtrace.rbegin(); iter != backtrace.rend(); ++iter) {
         // convert to world frame
         double wx, wy;
         costmap_->mapToWorld(iter->x(), iter->y(), wx, wy);
-        path.emplace_back(wx, wy);
+        path->emplace_back(wx, wy);
       }
       return true;
     }
 
     // explore neighbor of current node
-    for (const auto& motion : motions)
-    {
+    for (const auto& motion : motions_) {
       // explore a new node
       auto node_new = current + motion;
       node_new.set_g(current.g() + motion.g());
       node_new.set_id(grid2Index(node_new.x(), node_new.y()));
 
       // node_new in closed list
-      if (closed_list.find(node_new.id()) != closed_list.end())
+      if (closed_list.find(node_new.id()) != closed_list.end()) {
         continue;
+      }
 
       node_new.set_pid(current.id());
 
       // next node hit the boundary or obstacle
       // prevent planning failed when the current within inflation
       if ((node_new.id() < 0) || (node_new.id() >= map_size_) ||
-          (costmap_->getCharMap()[node_new.id()] >= costmap_2d::LETHAL_OBSTACLE * obstacle_factor_ &&
-           costmap_->getCharMap()[node_new.id()] >= costmap_->getCharMap()[current.id()]))
+          (costmap_->getCharMap()[node_new.id()] >=
+               costmap_2d::LETHAL_OBSTACLE * config_.obstacle_inflation_factor() &&
+           costmap_->getCharMap()[node_new.id()] >=
+               costmap_->getCharMap()[current.id()])) {
         continue;
+      }
 
       // if using dijkstra implementation, do not consider heuristics cost
-      if (!is_dijkstra_)
-        node_new.set_h(std::hypot(node_new.x() - goal_node.x(), node_new.y() - goal_node.y()));
+      if (!is_dijkstra_) {
+        node_new.set_h(
+            std::hypot(node_new.x() - goal_node.x(), node_new.y() - goal_node.y()));
+      }
 
       // if using GBFS implementation, only consider heuristics cost
-      if (is_gbfs_)
+      if (is_gbfs_) {
         node_new.set_g(0.0);
+      }
       // else, g will be calculate through node_new = current + m
 
       open_list.push(node_new);
