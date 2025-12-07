c5b1a3f9a287e587b711c992d0794c596fb48ed5
mr_winter <913982779@qq.com>
2025-12-07 16:22:52 +0800

    feat(planner/path_planner): proto-based Lazy-Theta* path planner

--- src/core/path_planner/path_planner/include/path_planner/graph_planner/lazy_theta_star_planner.h
+++ src/core/path_planner/path_planner/include/path_planner/graph_planner/lazy_theta_star_planner.h
@@ -14,20 +14,17 @@
  *
  * ********************************************************
  */
-#ifndef LAZY_THETA_STAR_H
-#define LAZY_THETA_STAR_H
+#ifndef RMP_PATH_PLANNER_GRAPH_PLANNER_LAZY_THETA_STAR_H
+#define RMP_PATH_PLANNER_GRAPH_PLANNER_LAZY_THETA_STAR_H
 
 #include "path_planner/graph_planner/theta_star_planner.h"
 
-namespace rmp
-{
-namespace path_planner
-{
+namespace rmp {
+namespace path_planner {
 /**
  * @brief Class for objects that plan using the lazy Theta* algorithm
  */
-class LazyThetaStarPathPlanner : public ThetaStarPathPlanner
-{
+class LazyThetaStarPathPlanner : public ThetaStarPathPlanner {
 private:
   using Node = rmp::common::structure::Node<int>;
 
@@ -35,19 +32,19 @@ public:
   /**
    * @brief Construct a new LaztThetaStar object
    * @param costmap   the environment for path planning
-   * @param obstacle_factor obstacle factor(greater means obstacles)
    */
-  LazyThetaStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double obstacle_factor = 1.0);
+  LazyThetaStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros);
 
   /**
    * @brief Lazy Theta* implementation
    * @param start         start node
    * @param goal          goal node
-   * @param path          optimal path consists of Node
+   * @param path          The resulting path in (x, y, theta)
    * @param expand        containing the node been search during the process
    * @return  true if path found, else false
    */
-  bool plan(const Point3d& start, const Point3d& goal, Points3d& path, Points3d& expand);
+  bool plan(const common::geometry::Point3d& start, const common::geometry::Point3d& goal,
+            common::geometry::Points3d* path, common::geometry::Points3d* expand);
 
 protected:
   /**
@@ -66,8 +63,10 @@ protected:
 private:
   std::unordered_map<int, Node> closed_list_;  // closed list
   const std::vector<Node> motions = {
-    { 0, 1, 1.0 },          { 1, 0, 1.0 },           { 0, -1, 1.0 },          { -1, 0, 1.0 },
-    { 1, 1, std::sqrt(2) }, { 1, -1, std::sqrt(2) }, { -1, 1, std::sqrt(2) }, { -1, -1, std::sqrt(2) },
+    { 0, 1, 1.0 },           { 1, 0, 1.0 },
+    { 0, -1, 1.0 },          { -1, 0, 1.0 },
+    { 1, 1, std::sqrt(2) },  { 1, -1, std::sqrt(2) },
+    { -1, 1, std::sqrt(2) }, { -1, -1, std::sqrt(2) },
   };
 };
 }  // namespace path_planner
--- src/core/path_planner/path_planner/src/graph_planner/lazy_theta_star_planner.cpp
+++ src/core/path_planner/path_planner/src/graph_planner/lazy_theta_star_planner.cpp
@@ -21,37 +21,31 @@
 #include "common/geometry/collision_checker.h"
 #include "path_planner/graph_planner/lazy_theta_star_planner.h"
 
-namespace rmp
-{
-namespace path_planner
-{
-namespace
-{
+using namespace rmp::common::geometry;
 using CollisionChecker = rmp::common::geometry::CollisionChecker;
-}
 
+namespace rmp {
+namespace path_planner {
 /**
  * @brief Construct a new LazyThetaStar object
  * @param costmap   the environment for path planning
- * @param obstacle_factor obstacle factor(greater means obstacles)
  */
-LazyThetaStarPathPlanner::LazyThetaStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros, double obstacle_factor)
-  : ThetaStarPathPlanner(costmap_ros, obstacle_factor){};
+LazyThetaStarPathPlanner::LazyThetaStarPathPlanner(costmap_2d::Costmap2DROS* costmap_ros)
+  : ThetaStarPathPlanner(costmap_ros){};
 
 /**
  * @brief Lazy Theta* implementation
  * @param start         start node
  * @param goal          goal node
- * @param path          optimal path consists of Node
+ * @param path          The resulting path in (x, y, theta)
  * @param expand        containing the node been search during the process
  * @return  true if path found, else false
  */
-bool LazyThetaStarPathPlanner::plan(const Point3d& start, const Point3d& goal, Points3d& path, Points3d& expand)
-{
+bool LazyThetaStarPathPlanner::plan(const Point3d& start, const Point3d& goal,
+                                    Points3d* path, Points3d* expand) {
   double m_start_x, m_start_y, m_goal_x, m_goal_y;
   if ((!validityCheck(start.x(), start.y(), m_start_x, m_start_y)) ||
-      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y)))
-  {
+      (!validityCheck(goal.x(), goal.y(), m_goal_x, m_goal_y))) {
     return false;
   }
 
@@ -61,16 +55,15 @@ bool LazyThetaStarPathPlanner::plan(const Point3d& start, const Point3d& goal, P
   goal_node.set_id(grid2Index(goal_node.x(), goal_node.y()));
   // initialize
   closed_list_.clear();
-  path.clear();
-  expand.clear();
+  path->clear();
+  expand->clear();
 
   // push the start node into open list
   std::priority_queue<Node, std::vector<Node>, Node::compare_cost> open_list;
   open_list.push(start_node);
 
   // main process
-  while (!open_list.empty())
-  {
+  while (!open_list.empty()) {
     // pop current node from open list
     auto current = open_list.top();
     open_list.pop();
@@ -85,30 +78,29 @@ bool LazyThetaStarPathPlanner::plan(const Point3d& start, const Point3d& goal, P
       continue;
 
     closed_list_.insert(std::make_pair(current.id(), current));
-    expand.emplace_back(current.x(), current.y());
+    expand->emplace_back(current.x(), current.y());
 
     // goal found
-    if (current == goal_node)
-    {
-      const auto& backtrace = _convertClosedListToPath<Node>(closed_list_, start_node, goal_node);
-      for (auto iter = backtrace.rbegin(); iter != backtrace.rend(); ++iter)
-      {
+    if (current == goal_node) {
+      const auto& backtrace =
+          _convertClosedListToPath<Node>(closed_list_, start_node, goal_node);
+      for (auto iter = backtrace.rbegin(); iter != backtrace.rend(); ++iter) {
         // convert to world frame
         double wx, wy;
         costmap_->mapToWorld(iter->x(), iter->y(), wx, wy);
-        path.emplace_back(wx, wy);
+        path->emplace_back(wx, wy);
       }
       return true;
     }
 
     // explore neighbor of current node
-    for (const auto& m : motions)
-    {
+    for (const auto& m : motions) {
       // explore a new node
       // path 1
       auto node_new = current + m;  // add the .x(), .y(), g_
       node_new.set_g(current.g() + m.g());
-      node_new.set_h(std::hypot(node_new.x() - goal_node.x(), node_new.y() - goal_node.y()));
+      node_new.set_h(
+          std::hypot(node_new.x() - goal_node.x(), node_new.y() - goal_node.y()));
       node_new.set_id(grid2Index(node_new.x(), node_new.y()));
       node_new.set_pid(current.id());
 
@@ -118,7 +110,8 @@ bool LazyThetaStarPathPlanner::plan(const Point3d& start, const Point3d& goal, P
 
       // next node hit the boundary or obstacle
       if ((node_new.id() < 0) || (node_new.id() >= map_size_) ||
-          (costmap_->getCharMap()[node_new.id()] >= costmap_2d::LETHAL_OBSTACLE * obstacle_factor_ &&
+          (costmap_->getCharMap()[node_new.id()] >=
+               costmap_2d::LETHAL_OBSTACLE * config_.obstacle_inflation_factor() &&
            costmap_->getCharMap()[node_new.id()] >= costmap_->getCharMap()[current.id()]))
         continue;
 
@@ -130,8 +123,7 @@ bool LazyThetaStarPathPlanner::plan(const Point3d& start, const Point3d& goal, P
       parent.set_x(tmp_x);
       parent.set_y(tmp_y);
       auto find_parent = closed_list_.find(parent.id());
-      if (find_parent != closed_list_.end())
-      {
+      if (find_parent != closed_list_.end()) {
         parent = find_parent->second;
         // path 2
         _updateVertex(parent, node_new);
@@ -149,12 +141,10 @@ bool LazyThetaStarPathPlanner::plan(const Point3d& start, const Point3d& goal, P
  * @param parent
  * @param child
  */
-void LazyThetaStarPathPlanner::_updateVertex(const Node& parent, Node& child)
-{
+void LazyThetaStarPathPlanner::_updateVertex(const Node& parent, Node& child) {
   // path 2
   const double dist = std::hypot(parent.x() - child.x(), parent.y() - child.y());
-  if (parent.g() + dist < child.g())
-  {
+  if (parent.g() + dist < child.g()) {
     child.set_g(parent.g() + dist);
     child.set_pid(parent.id());
   }
@@ -164,8 +154,7 @@ void LazyThetaStarPathPlanner::_updateVertex(const Node& parent, Node& child)
  * @brief check if the parent of vertex need to be updated. if so, update it
  * @param node
  */
-void LazyThetaStarPathPlanner::_setVertex(Node& node)
-{
+void LazyThetaStarPathPlanner::_setVertex(Node& node) {
   // get the coordinate of parent node
   Node parent;
   parent.set_id(node.pid());
@@ -181,28 +170,27 @@ void LazyThetaStarPathPlanner::_setVertex(Node& node)
   parent = find_parent->second;
 
   auto isCollision = [&](const Node& node1, const Node& node2) {
-    return CollisionChecker::BresenhamCollisionDetection(node1, node2, [&](const Node& node) {
-      return costmap_->getCharMap()[grid2Index(node.x(), node.y())] >= costmap_2d::LETHAL_OBSTACLE * obstacle_factor_;
-    });
+    return CollisionChecker::BresenhamCollisionDetection(
+        node1, node2, [&](const Node& node) {
+          return costmap_->getCharMap()[grid2Index(node.x(), node.y())] >=
+                 costmap_2d::LETHAL_OBSTACLE * config_.obstacle_inflation_factor();
+        });
   };
 
-  if (isCollision(parent, node))
-  {
+  if (isCollision(parent, node)) {
     // path 1
     node.set_g(std::numeric_limits<double>::max());
-    for (const auto& m : motions)
-    {
+    for (const auto& m : motions) {
       auto parent_new = node + m;
       parent_new.set_id(grid2Index(parent_new.x(), parent_new.y()));
       auto find_parent_new = closed_list_.find(parent_new.id());
 
-      if (find_parent_new != closed_list_.end())
-      {
+      if (find_parent_new != closed_list_.end()) {
         // parent_new exists in closed list
         parent_new = find_parent_new->second;
-        const double dist = std::hypot(parent_new.x() - node.x(), parent_new.y() - node.y());
-        if (parent_new.g() + dist < node.g())
-        {
+        const double dist =
+            std::hypot(parent_new.x() - node.x(), parent_new.y() - node.y());
+        if (parent_new.g() + dist < node.g()) {
           node.set_g(parent_new.g() + dist);
           node.set_pid(parent_new.id());
         }
